53b8576e230c2b48f9771e50f3e6d828fdad79c8
David Whittington <djwhitt@gmail.com>
2022-04-13 13:49:02 -0500

    Use raw better-sqlite3 to maximize insert performance

--- index.js
+++ index.js
@@ -2,26 +2,40 @@ const crypto = require('crypto');
 const express = require('express');
 const bodyParser = require('body-parser');
 const R = require('ramda');
+const Database = require('better-sqlite3');
 
 const port = 3000;
 const app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 
-const knex = require('knex')({
-  client: 'better-sqlite3',
-  useNullAsDefault: true,
-  connection: {
-    filename: "./chain.db"
-  }
-});
+//const knex = require('knex')({
+//  client: 'better-sqlite3',
+//  useNullAsDefault: true,
+//  connection: {
+//    filename: "./chain.db"
+//  }
+//});
 
-async function saveTransactions(txs) {
-  const txInserts = [];
-  const tagInserts = [];
-  const transactionTagInserts = [];
+db = new Database('./chain.db');
 
+const txInsertStmt = db.prepare(`
+  INSERT OR IGNORE INTO stable_transactions (id, height)
+  VALUES (@id, @height)
+`);
+
+const tagInsertStmt = db.prepare(`
+  INSERT OR IGNORE INTO tags (hash, name, value)
+  VALUES (@hash, @name, @value)
+`);
+
+const transactionTagInsertsStmt = db.prepare(`
+  INSERT OR IGNORE INTO stable_transaction_tags (tag_hash, height, transaction_id)
+  VALUES (@tag_hash, @height, @transaction_id)
+`);
+
+const insertTransactions = db.transaction((txs) => {
   for (tx of txs) {
-    txInserts.push({
+    txInsertStmt.run({
       id: tx.id,
       height: tx.BlockHeight,
     });
@@ -29,48 +43,24 @@ async function saveTransactions(txs) {
     for (tag of tx.tags) {
       const tagHashContent = `${tag.name}|${tag.value}`;
       const tagHash = crypto.createHash('md5').update(tagHashContent).digest('hex');
-      tagInserts.push({
+
+      tagInsertStmt.run({
         hash: tagHash,
         name: tag.name,
         value: tag.value,
       });
 
-      transactionTagInserts.push({
+      transactionTagInsertsStmt.run({
         tag_hash: tagHash,
         height: tx.BlockHeight,
         transaction_id: tx.id
       });
     }
   }
+});
 
-  await knex.transaction(async (trx) => {
-    for (txInsertsChunk of R.splitEvery(100, txInserts)) {
-      await knex('stable_transactions')
-        .transacting(trx)
-        .insert(txInsertsChunk)
-        .onConflict('id').ignore();
-    }
-
-    if (tagInserts.length > 0) {
-      for (tagInsertsChunk of R.splitEvery(100, tagInserts)) {
-        await knex('tags')
-          .transacting(trx)
-          .insert(tagInsertsChunk)
-          .onConflict('hash')
-          .ignore();
-      }
-    }
-
-    if (transactionTagInserts.length > 0) {
-      for (transactionTagInsertsChunk of R.splitEvery(100, transactionTagInserts)) {
-        await knex('stable_transaction_tags')
-          .transacting(trx)
-          .insert(transactionTagInsertsChunk)
-          .onConflict(['tag_hash', 'height', 'transaction_id'])
-          .ignore();
-      }
-    }
-  });
+function saveTransactions(txs) {
+  insertTransactions(txs);
 }
 
 let totalTxCount = 0;
@@ -80,7 +70,7 @@ app.post('/add-transactions', async (req, res) => {
   totalTxCount += txCount;
   console.log(`Received ${txCount} transactions, total: ${totalTxCount}`);
   const startTs = Date.now();
-  await saveTransactions(req.body);
+  saveTransactions(req.body);
   console.log(`Saved ${txCount} transactions in ${Date.now() - startTs}ms`);
   res.send({ endpoint: 'add-transactions' });
 });
