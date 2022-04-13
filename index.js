736ff14a8c92256fb52e8753af78a7c869960ff8
David Whittington <djwhitt@gmail.com>
2022-04-12 19:34:27 -0500

    WIP SQLite performance validation work

--- index.js
+++ index.js
@@ -1,11 +1,95 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const crypto = require('crypto');
+const express = require('express');
+const bodyParser = require('body-parser');
+const R = require('ramda');
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!')
-})
+const port = 3000;
+const app = express();
+app.use(bodyParser.json({limit: '50mb'}));
+
+const knex = require('knex')({
+  client: 'better-sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: "./chain.db"
+  }
+});
+
+async function saveTransactions(txs) {
+  const txInserts = [];
+  const tagInserts = [];
+  const transactionTagInserts = [];
+
+  for (tx of txs) {
+    txInserts.push({
+      id: tx.id,
+      height: tx.BlockHeight,
+    });
+
+    for (tag of tx.tags) {
+      const tagHashContent = `${tag.name}|${tag.value}`;
+      const tagHash = crypto.createHash('md5').update(tagHashContent).digest('hex');
+      tagInserts.push({
+        hash: tagHash,
+        name: tag.name,
+        value: tag.value,
+      });
+
+      transactionTagInserts.push({
+        tag_hash: tagHash,
+        height: tx.BlockHeight,
+        transaction_id: tx.id
+      });
+    }
+  }
+
+  await knex.transaction(async (trx) => {
+    for (txInsertsChunk of R.splitEvery(100, txInserts)) {
+      await knex('stable_transactions')
+        .transacting(trx)
+        .insert(txInsertsChunk)
+        .onConflict('id').ignore();
+    }
+
+    if (tagInserts.length > 0) {
+      for (tagInsertsChunk of R.splitEvery(100, tagInserts)) {
+        await knex('tags')
+          .transacting(trx)
+          .insert(tagInsertsChunk)
+          .onConflict('hash')
+          .ignore();
+      }
+    }
+
+    if (transactionTagInserts.length > 0) {
+      for (transactionTagInsertsChunk of R.splitEvery(100, transactionTagInserts)) {
+        await knex('stable_transaction_tags')
+          .transacting(trx)
+          .insert(transactionTagInsertsChunk)
+          .onConflict(['tag_hash', 'height', 'transaction_id'])
+          .ignore();
+      }
+    }
+  });
+}
+
+let totalTxCount = 0;
+
+app.post('/add-transactions', async (req, res) => {
+  txCount = req.body.length;
+  totalTxCount += txCount;
+  console.log(`Received ${txCount} transactions, total: ${totalTxCount}`);
+  const startTs = Date.now();
+  await saveTransactions(req.body);
+  console.log(`Saved ${txCount} transactions in ${Date.now() - startTs}ms`);
+  res.send({ endpoint: 'add-transactions' });
+});
+
+app.post('/add-blocks', (_req, res) => {
+  //console.log(req.body);
+  res.send({ endpoint: 'add-blocks' });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- schema.sql
+++ schema.sql
@@ -1,77 +1,80 @@
-CREATE TABLE stable_blocks (
-  -- Identity
-  height INTEGER PRIMARY KEY,
-  indep_hash BLOB UNIQUE,
-  previous_block BLOB,
-  nonce BLOB NOT NULL,
-  hash BLOB NOT NULL,
-  block_timestamp INTEGER NOT NULL,
-
-  -- Difficulty
-  diff TEXT NOT NULL,
-  cumulative_diff TEXT,
-  last_retarget TEXT NOT NULL,
-
-  -- Rewards
-  reward_addr BLOB,
-  reward_pool TEXT NOT NULL,
-
-  -- Sizes
-  block_size INTEGER NOT NULL,
-  weave_size INTEGER NOT NULL,
-
-  -- Pricing
-  usd_to_ar_rate_dividend INTEGER,
-  usd_to_ar_rate_divisor INTEGER,
-  scheduled_usd_to_ar_rate_dividend INTEGER,
-  scheduled_usd_to_ar_rate_divisor INTEGER,
-
-  -- Packing
-  packing_2_5_threshold INTEGER,
-  strict_data_split_threshold INTEGER,
-
-  -- Hash list merkel
-  hash_list_merkle BLOB,
-
-  -- Wallets
-  wallet_list BLOB,
-
-  -- Transactions
-  tx_root BLOB
-);
+--CREATE TABLE stable_blocks (
+--  -- Identity
+--  height INTEGER PRIMARY KEY,
+--  indep_hash BLOB UNIQUE,
+--  previous_block BLOB,
+--  nonce BLOB NOT NULL,
+--  hash BLOB NOT NULL,
+--  block_timestamp INTEGER NOT NULL,
+--
+--  -- Difficulty
+--  diff TEXT NOT NULL,
+--  cumulative_diff TEXT,
+--  last_retarget TEXT NOT NULL,
+--
+--  -- Rewards
+--  reward_addr BLOB,
+--  reward_pool TEXT NOT NULL,
+--
+--  -- Sizes
+--  block_size INTEGER NOT NULL,
+--  weave_size INTEGER NOT NULL,
+--
+--  -- Pricing
+--  usd_to_ar_rate_dividend INTEGER,
+--  usd_to_ar_rate_divisor INTEGER,
+--  scheduled_usd_to_ar_rate_dividend INTEGER,
+--  scheduled_usd_to_ar_rate_divisor INTEGER,
+--
+--  -- Packing
+--  packing_2_5_threshold INTEGER,
+--  strict_data_split_threshold INTEGER,
+--
+--  -- Hash list merkel
+--  hash_list_merkle BLOB,
+--
+--  -- Wallets
+--  wallet_list BLOB,
+--
+--  -- Transactions
+--  tx_root BLOB
+--);
 
 CREATE TABLE stable_transactions (
   -- Identity
-  id BLOB PRIMARY KEY,
-  signature BLOB NOT NULL,
-  height INTEGER NOT NULL,
-  block_transaction_index INTEGER NOT NULL,
-  format INTEGER NOT NULL,
-  last_tx BLOB NOT NULL,
+  id TEXT PRIMARY KEY,
+  height INTEGER NOT NULL
+  --signature BLOB NOT NULL,
+  --block_transaction_index INTEGER NOT NULL,
+  --format INTEGER NOT NULL,
+  --last_tx BLOB NOT NULL,
 
   -- Ownership
-  owner_address BLOB NOT NULL,
-  target BLOB,
+  --owner_address BLOB NOT NULL,
+  --target BLOB,
 
   -- Tokens
-  quantity TEXT NOT NULL,
-  reward TEXT NOT NULL,
+  --quantity TEXT NOT NULL,
+  --reward TEXT NOT NULL,
 
   -- Data
-  data_size INTEGER,
-  data_root BLOB
+  --data_size INTEGER,
+  --data_root BLOB
 );
 
+CREATE INDEX stable_transactions_id_height_idx ON stable_transactions (id, height);
+
 CREATE TABLE tags (
-  hash BLOB PRIMARY KEY,
+  hash TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   value TEXT NOT NULL
 );
 
+-- TODO add block_transaction_index
 CREATE TABLE stable_transaction_tags (
-  tag_hash BLOB NOT NULL,
+  tag_hash TEXT NOT NULL,
   height INTEGER NOT NULL,
   transaction_id BLOB NOT NULL,
-  transaction_tag_index INTEGER NOT NULL,
-  PRIMARY KEY (tag_hash, height, transaction_id, transaction_tag_index)
+  --transaction_tag_index INTEGER NOT NULL,
+  PRIMARY KEY (tag_hash, height, transaction_id)
 );
