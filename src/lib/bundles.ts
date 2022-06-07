eac51c2af79f46daebc67ffc125445c9d9a420e8
David Whittington <djwhitt@gmail.com>
2022-06-07 15:32:15 -0500

    Pass batchSize into bundle import functions

--- src/lib/bundles.ts
+++ src/lib/bundles.ts
@@ -3,26 +3,32 @@ import stream from 'stream';
 
 import { IBundleDatabase } from '../types.js';
 
+const DEFAULT_BATCH_SIZE = 10;
+
 export async function importAns102Bundle({
   log,
   db,
   bundleStream,
-  parentTxId
+  parentTxId,
+  batchSize = DEFAULT_BATCH_SIZE
 }: {
   log: winston.Logger;
   db: IBundleDatabase;
   bundleStream: stream.Readable;
   parentTxId: string;
+  batchSize?: number;
 }) {}
 
 export async function importAns104Bundle({
   log,
   db,
   bundleStream,
-  parentTxId
+  parentTxId,
+  batchSize = DEFAULT_BATCH_SIZE
 }: {
   log: winston.Logger;
   db: IBundleDatabase;
   bundleStream: stream.Readable;
   parentTxId: string;
+  batchSize?: number;
 }) {}
