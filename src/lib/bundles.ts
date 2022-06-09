b235a930b18d97468b8f315737d5008644dcc4e7
dtfiedler <dylan@ar.io>
2022-06-09 12:36:20 -0600

    feat:add arbundles and write psuedo code for ans104 importing

--- src/lib/bundles.ts
+++ src/lib/bundles.ts
@@ -1,10 +1,12 @@
 import * as winston from 'winston';
 import stream from 'stream';
-
+import processBundle from "arbundles/stream/index.js";
 import { IBundleDatabase } from '../types.js';
 
 const DEFAULT_BATCH_SIZE = 10;
 
+/* eslint-disable */
+// @ts-ignore
 export async function importAns102Bundle({
   log,
   db,
@@ -31,4 +33,22 @@ export async function importAns104Bundle({
   bundleStream: stream.Readable;
   parentTxId: string;
   batchSize?: number;
-}) {}
+}) {
+  const alreadyProcessed = new Set();
+  const txBatch = [];
+  const tagsBatch = [];
+
+  const iterable = await processBundle.default(bundleStream)
+  const bundleLength = iterable.length;
+
+  log.info(`[ans-104-bundle] processing ${parentTxId} bundle of size ${bundleLength}`)
+
+  // Save individual data items to database
+
+  // Traverse tags of data items
+
+  // Update data items to S3
+
+  // Store tagsBatch in array (where is this used?) 
+
+}
