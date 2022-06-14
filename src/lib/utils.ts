26fff38fb53758649ee1608b3d11226598b8db16
dtfiedler <dylan@ar.io>
2022-06-14 11:13:21 -0600

    feat: add some basic unit tests for b64 util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,13 @@
 import { createHash } from 'crypto';
 
-export function fromB64Url(input: string) {
+export function fromB64Url(input: string): Buffer {
   return Buffer.from(input, 'base64');
 }
 
-export function toB64url(buffer: Buffer) {
+export function toB64url(buffer: Buffer): string {
   return buffer.toString('base64url');
 }
 
-export function sha256B64Url(input: Buffer) {
+export function sha256B64Url(input: Buffer): string {
   return toB64url(createHash('sha256').update(input).digest());
 }
