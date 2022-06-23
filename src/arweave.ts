d6ab855ff5b13fbc9ba212288e23bceb4a775a81
David Whittington <djwhitt@gmail.com>
2022-06-23 16:12:21 -0500

    feat: prefetch more blocks + retry failed requests
    Also consolidates request handling in on place in preparation for
    outgoing request rate limiting.
    
    PE-1739

--- src/arweave.ts
+++ src/arweave.ts
@@ -1,5 +1,6 @@
 import { default as NodeCache } from 'node-cache';
-import { default as axios } from 'axios';
+import { AxiosRequestConfig, AxiosResponse, default as axios } from 'axios';
+import * as rax from 'retry-axios';
 
 import { IChainSource, JsonBlock, JsonTransaction } from './types.js';
 
@@ -11,24 +12,50 @@ type Peer = {
 };
 
 export class ChainApiClient implements IChainSource {
-  private chainApiUrl: string;
+  private trustedNodeUrl: string;
+  private trustedNodeAxios;
   private peers: Record<string, Peer> = {};
-  private blockPromiseCache = new NodeCache({ stdTTL: 300, useClones: false });
+  private blockByHeightPromiseCache = new NodeCache({
+    checkperiod: 10,
+    stdTTL: 30,
+    useClones: false
+  });
   private maxHeight = -1;
+  private blockPrefetchCount = 50;
 
   constructor(chainApiUrl: string) {
-    this.chainApiUrl = chainApiUrl.replace(/\/$/, '');
+    this.trustedNodeUrl = chainApiUrl.replace(/\/$/, '');
+    this.trustedNodeAxios = axios.create({
+      baseURL: this.trustedNodeUrl,
+      timeout: 15000
+    });
+    this.trustedNodeAxios.defaults.raxConfig = {
+      retry: 5,
+      instance: this.trustedNodeAxios
+    };
+    rax.attach(this.trustedNodeAxios);
   }
 
+  trustedNodeRequest(request: AxiosRequestConfig) {
+    return this.trustedNodeAxios(request);
+  }
+
+  // TODO recursively traverse peers
   async refreshPeers(): Promise<void> {
     try {
-      const response = await axios.get(`${this.chainApiUrl}/peers`);
+      const response = await this.trustedNodeRequest({
+        method: 'GET',
+        url: '/peers'
+      });
       const peerHosts = response.data as string[];
       await Promise.all(
         peerHosts.map(async (peerHost) => {
           try {
             const peerUrl = `http://${peerHost}`;
-            const response = await axios.get(`${peerUrl}/info`);
+            const response = await this.trustedNodeRequest({
+              method: 'GET',
+              url: '/info'
+            });
             this.peers[peerHost] = {
               url: peerUrl,
               blocks: response.data.blocks,
@@ -41,56 +68,80 @@ export class ChainApiClient implements IChainSource {
           return;
         })
       );
-      console.log(this.peers);
     } catch (error) {
       // TODO track metric
     }
   }
 
-  async prefetchBlockByHeight(height: number): Promise<void> {
-    try {
-      const cachedResponsePromise = this.blockPromiseCache.get(height);
-      if (cachedResponsePromise) {
-        // Update TTL if block promise is already cached
-        this.blockPromiseCache.set(height, cachedResponsePromise);
-        return;
+  prefetchBlockByHeight(height: number) {
+    const cachedResponsePromise = this.blockByHeightPromiseCache.get(height);
+    if (cachedResponsePromise) {
+      // Update TTL if block promise is already cached
+      try {
+        this.blockByHeightPromiseCache.set(height, cachedResponsePromise);
+      } catch (error) {
+        // TODO log error
       }
-      const responsePromise = axios.get(
-        `${this.chainApiUrl}/block/height/${height}`
-      );
-      this.blockPromiseCache.set(height, responsePromise);
-    } catch (error) {
-      // TODO log error
-    }
-  }
-
-  // TODO handle errors (retry 429s and 5XXs)
-  async getBlockByHeight(height: number): Promise<JsonBlock> {
-    if (height < this.maxHeight) {
-      this.prefetchBlockByHeight(height + 1);
+      return;
     }
 
-    const response = await (this.blockPromiseCache.has(height)
-      ? this.blockPromiseCache.get(height)
-      : axios.get(`${this.chainApiUrl}/block/height/${height}`));
+    const responsePromise = this.trustedNodeRequest({
+      method: 'GET',
+      url: `/block/height/${height}`
+    }).catch((error) => {
+      return error;
+    });
+    this.blockByHeightPromiseCache.set(height, responsePromise);
 
-    // TODO throw if response is undefined
-    // TODO fix type
-    return (response as any).data as JsonBlock;
+    // TODO prefetch txs
   }
 
-  // TODO handle errors (retry 429s and 5XXs)
+  async getBlockByHeight(
+    height: number,
+    shouldPrefetch = false
+  ): Promise<JsonBlock> {
+    // Prefetch the requested block
+    this.prefetchBlockByHeight(height);
+
+    // Prefetch the next N blocks
+    if (shouldPrefetch && height < this.maxHeight) {
+      for (let i = 1; i <= this.blockPrefetchCount; i++) {
+        const prefetchHeight = height + i;
+        if (prefetchHeight <= this.maxHeight) {
+          this.prefetchBlockByHeight(prefetchHeight);
+        } else {
+          break;
+        }
+      }
+    }
+
+    const responseOrError = await this.blockByHeightPromiseCache.get(height);
+
+    // TODO cleanup error handling
+    if (!(responseOrError as AxiosResponse).status) {
+      throw new Error(`Failed to retrieve block at height ${height}`);
+    }
+
+    return (responseOrError as AxiosResponse).data as JsonBlock;
+  }
+
   async getTx(txId: string): Promise<JsonTransaction> {
-    const response = await axios.get(`${this.chainApiUrl}/tx/${txId}`);
+    const response = await this.trustedNodeRequest({
+      method: 'GET',
+      url: `/tx/${txId}`
+    });
     return response.data as JsonTransaction;
   }
 
-  async getBlockAndTxs(height: number): Promise<{
+  async getBlockAndTxs(
+    height: number,
+    shouldPrefetch = true
+  ): Promise<{
     block: JsonBlock;
     txs: JsonTransaction[];
     missingTxIds: string[];
   }> {
-    const block = await this.getBlockByHeight(height);
+    const block = await this.getBlockByHeight(height, shouldPrefetch);
 
     // Retrieve block transactions
     const missingTxIds: string[] = [];
@@ -111,7 +162,10 @@ export class ChainApiClient implements IChainSource {
   }
 
   async getHeight(): Promise<number> {
-    const response = await axios.get(`${this.chainApiUrl}/height`);
+    const response = await this.trustedNodeRequest({
+      method: 'GET',
+      url: '/height'
+    });
     this.maxHeight =
       this.maxHeight < response.data ? response.data : this.maxHeight;
     return response.data as number;
