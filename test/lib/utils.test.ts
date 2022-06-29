ba12e08e617cbde821c6977f22bac8d3803f5cdf
Dylan Fiedler <dtfiedler@users.noreply.github.com>
2022-06-29 16:28:21 -0600

    chore(utils): improve tests for b64 util functions (#3)
    Co-authored-by: dtfiedler <dylan@ar.io>

--- test/lib/utils.test.ts
+++ test/lib/utils.test.ts
@@ -1,42 +1,41 @@
 import { expect } from 'chai';
-import { createHash } from 'crypto';
 import { fromB64Url, sha256B64Url, toB64url } from '../../src/lib/utils.js';
 
+const TEST_STRING = 'http://test.com';
+const TEST_BASE_64_URL_ENCODED_STRING = 'aHR0cDovL3Rlc3QuY29t';
+const TEST_BASE_64_BUFFER = Buffer.from(TEST_STRING);
+const TEST_BASE_64_BUFFER_WITH_PADDING = Buffer.from(
+  TEST_STRING.padStart(10).padEnd(10)
+);
+const TEST_BASE_64_SHA_256_STRING =
+  'i0CKDHFj_f_wbO0-gNfSs6zZ25AJBcR4PCgpW4yZYWU';
+
 describe('fromB64Url', () => {
-  it('should convert string to base64 buffer', () => {
-    const str = 'test';
-    const result = fromB64Url(str);
-    const expected = Buffer.from(str, 'base64');
-    expect(result).to.deep.equal(expected);
+  it('should convert base64 url encoded string to buffer', () => {
+    expect(fromB64Url(TEST_BASE_64_URL_ENCODED_STRING)).to.deep.equal(
+      TEST_BASE_64_BUFFER
+    );
   });
 });
 
 describe('toB64url', () => {
-  it('should convert buffer to base64url string', () => {
-    const str = Buffer.from('test', 'base64');
-    const result = toB64url(str);
-    const expected = str.toString('base64url');
-    expect(result).to.equal(expected);
+  it('should convert buffer to a base64url encoded string', () => {
+    expect(toB64url(TEST_BASE_64_BUFFER)).to.equal(
+      TEST_BASE_64_URL_ENCODED_STRING
+    );
   });
 
-  it('should properly handle padding', () => {
-    const str = 'test';
-    const buf = Buffer.from(str, 'base64');
-    const paddedBuffer = Buffer.concat([buf], 32);
-    const result = toB64url(paddedBuffer);
-    const expected = paddedBuffer.toString('base64url');
-    expect(result).to.deep.equal(expected);
+  it('should convert a buffer with padding to base64 url encoded string', () => {
+    expect(toB64url(TEST_BASE_64_BUFFER_WITH_PADDING)).to.deep.equal(
+      TEST_BASE_64_URL_ENCODED_STRING
+    );
   });
 });
 
 describe('sha256B64Url', () => {
   it('should convert buffer to sha256 string', () => {
-    const str = Buffer.from('test', 'base64');
-    const result = sha256B64Url(str);
-    const expected = createHash('sha256')
-      .update(str)
-      .digest()
-      .toString('base64url');
-    expect(result).to.equal(expected);
+    expect(sha256B64Url(TEST_BASE_64_BUFFER)).to.deep.equal(
+      TEST_BASE_64_SHA_256_STRING
+    );
   });
 });
