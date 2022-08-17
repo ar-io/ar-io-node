fa3fd714ec656c555f72ec1b23b27280dd1b99cc
David Whittington <djwhitt@gmail.com>
2022-08-17 10:35:38 -0500

    test(encoding): add tx msgpack encode/decode tests PE-1929

--- test/lib/encoding.test.ts
+++ test/lib/encoding.test.ts
@@ -1,12 +1,20 @@
 import { expect } from 'chai';
+import msgpack from 'msgpack-lite';
 
 import {
   b64UrlToUtf8,
   fromB64Url,
+  fromMsgpack,
+  jsonTxToMsgpack,
+  jsonTxToMsgpackTx,
+  msgpackToJsonTx,
+  msgpackTxToJsonTx,
   sha256B64Url,
   toB64Url,
+  toMsgpack,
   utf8ToB64Url,
 } from '../../src/lib/encoding.js';
+import { ArweaveChainSourceStub } from '../stubs.js';
 
 const TEST_STRING = 'http://test.com';
 const TEST_BASE_64_URL_ENCODED_STRING = 'aHR0cDovL3Rlc3QuY29t';
@@ -17,9 +25,9 @@ const TEST_BASE_64_BUFFER_WITH_PADDING = Buffer.from(
 const TEST_BASE_64_SHA_256_STRING =
   'i0CKDHFj_f_wbO0-gNfSs6zZ25AJBcR4PCgpW4yZYWU';
 
-describe('Encoding functions', () => {
+describe('Base64 URL encoding functions', () => {
   describe('fromB64Url', () => {
-    it('should convert base64 url encoded string to buffer', () => {
+    it('should convert a base64 url encoded string to buffer', () => {
       expect(fromB64Url(TEST_BASE_64_URL_ENCODED_STRING)).to.deep.equal(
         TEST_BASE_64_BUFFER,
       );
@@ -61,6 +69,81 @@ describe('Encoding functions', () => {
       expect(b64UrlToUtf8(TEST_BASE_64_URL_ENCODED_STRING)).to.deep.equal(
         TEST_STRING,
       );
+    });
+  });
+});
+
+describe('Message pack encoding and decoding functions', () => {
+  describe('toMsgpack and fromMsgpack', () => {
+    it('should round trip to and from MessagePack', () => {
+      const testObject = {
+        test: 'test',
+        test2: [1, 2, 3],
+      };
+
+      const testBuffer = toMsgpack(testObject);
+      const testObject2 = fromMsgpack(testBuffer);
+
+      expect(testObject2).to.deep.equal(testObject);
+    });
+
+    it("should preserve compatibility with 'standard' Message Pack", () => {
+      const testObject = {
+        test: 'test',
+        test2: [1, 2, 3],
+      };
+
+      const testBuffer = toMsgpack(testObject);
+      const testObject2 = msgpack.decode(testBuffer);
+
+      expect(testObject2).to.deep.equal(testObject);
+
+      const testBuffer2 = msgpack.encode(testObject);
+      const testObject3 = fromMsgpack(testBuffer2);
+
+      expect(testObject3).to.deep.equal(testObject);
+    });
+  });
+});
+
+describe('Transaction message pack encoding and decoding functions', () => {
+  describe('jsonTxToMsgpackTx and msgpackTxToJsonTx', () => {
+    it('should round trip to and from a MsgpackTransaction', async () => {
+      // TODO add transactions with more fields
+
+      const chainSource = new ArweaveChainSourceStub();
+      const tx = await chainSource.getTx(
+        'cK9WF2XMwFj5TF1uhaCSdrA2mVoaxAz20HkDyQhq0i0',
+      );
+
+      // Remove extranious header fields
+      delete (tx as any).data;
+      delete (tx as any).data_tree;
+
+      const msgpackTx = jsonTxToMsgpackTx(tx);
+      const jsonTx = msgpackTxToJsonTx(msgpackTx);
+
+      expect(jsonTx).to.deep.equal(tx);
+    });
+  });
+
+  describe('jsonTxToMsgpack and msgpackToJsonTx', () => {
+    it('should round trip to and from a MessagePack', async () => {
+      // TODO add transactions with more fields
+
+      const chainSource = new ArweaveChainSourceStub();
+      const tx = await chainSource.getTx(
+        'cK9WF2XMwFj5TF1uhaCSdrA2mVoaxAz20HkDyQhq0i0',
+      );
+
+      // Remove extranious header fields
+      delete (tx as any).data;
+      delete (tx as any).data_tree;
+
+      const buffer = jsonTxToMsgpack(tx);
+      const jsonTx = msgpackToJsonTx(buffer);
+
+      expect(jsonTx).to.deep.equal(tx);
     });
   });
 });
