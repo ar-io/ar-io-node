d5dfd0586ba4bdd587dd0537ad2fc65d6b46dc4b
David Whittington <djwhitt@gmail.com>
2022-08-23 15:34:47 -0500

    feat(migrations sqlite): add migration to setup initial schema PE-1950
    Uses CREATE TABLE IF NOT EXISTS so that it can be run against existing
    (pre migration framework) DBs.

---
+++ migrations/down/2022.08.23T18.24.52.core.init-schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE wallets;
+DROP TABLE stable_blocks;
+DROP TABLE stable_block_transactions;
+DROP TABLE stable_transactions;
+DROP TABLE missing_transactions;
+DROP TABLE tag_names;
+DROP TABLE tag_values;
+DROP TABLE stable_transaction_tags;
+DROP TABLE new_blocks;
+DROP TABLE new_block_heights;
+DROP TABLE new_transactions;
+DROP TABLE new_block_transactions;
+DROP TABLE new_transaction_tags;
