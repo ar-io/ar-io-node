3184b5981de67002adf4acec0ec4d5b8fbcb1ce7
dtfiedler <dylan@ar.io>
2022-08-31 08:56:37 -0500

    fix: implmement better stream logic to avoid loading tx data into buffers

--- src/arweave/tx-client.ts
+++ src/arweave/tx-client.ts
@@ -5,31 +5,28 @@ import { ChainSource, ChunkSource, TxDataSource } from '../types.js';
 
 export class TxClient implements TxDataSource {
   private log: winston.Logger;
-  private chunkSource: ChunkSource;
-  private chainSource: ChainSource;
+  // TODO: could make this a speicifc type
+  private client: ChainSource & ChunkSource;
 
   constructor({
     log,
-    chainSource,
-    chunkSource,
+    client,
   }: {
     log: winston.Logger;
-    chainSource: ChainSource;
-    chunkSource: ChunkSource;
+    // TODO: these could be one client that implements both (e.g. composite)
+    client: ChainSource & ChunkSource;
   }) {
     this.log = log.child({ client: 'tx-client' });
-    this.chainSource = chainSource;
-    this.chunkSource = chunkSource;
+    this.client = client;
   }
 
   async getTxData(txId: string): Promise<{ data: Readable; size: number }> {
     this.log.info('Fetching chunk data for tx', { txId });
 
     try {
-      const { offset, size } = await this.chainSource.getTxOffset(txId);
+      const { offset, size } = await this.client.getTxOffset(txId);
       const startOffset = +offset - +size + 1;
-      let chunkPromise =
-        this.chunkSource.getChunkDataByAbsoluteOffset(startOffset);
+      let chunkPromise = this.client.getChunkDataByAbsoluteOffset(startOffset);
       let bytes = 0;
       const data = new Readable({
         autoDestroy: true,
@@ -40,19 +37,18 @@ export class TxClient implements TxDataSource {
             }
 
             const chunkData = await chunkPromise;
-            // TODO: is this the best way to read the process returned stream
-            const chunk = chunkData.read();
-            bytes += chunk.length;
+            chunkData.on('data', (chunk) => {
+              this.push(chunk);
+              bytes += chunk.length;
+            });
 
             // we're not done gatehering all chunks yet
             if (bytes < size) {
               // TODO: fix scoping issue
-              chunkPromise = this.chunkSource
+              chunkPromise = this.client
                 .getChunkDataByAbsoluteOffset(startOffset + bytes)
                 .catch(() => ({}));
             }
-
-            this.push(chunk);
           } catch (error) {
             this.destroy();
           }
