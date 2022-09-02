a4ddd22bf22f8c3a33b4e5c90fc6c8a5cf9d1965
David Whittington <djwhitt@gmail.com>
2022-09-02 14:06:29 -0500

    chore(validation): improve consistency of error messages PE-2200

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -5,28 +5,31 @@ import {
 } from '../types.js';
 
 export function sanityCheckBlock(block: PartialJsonBlock) {
-  if (!block?.indep_hash) {
-    throw new Error('Invalid block: missing indep_hash');
+  if (!block.indep_hash) {
+    throw new Error("Invalid block: missing 'indep_hash'");
   }
 
-  if (!block?.height === undefined) {
-    console.log(block);
-    throw new Error('Invalid block: missing height');
+  if (!block.height === undefined) {
+    throw new Error("Invalid block: missing 'height'");
   }
 }
 
 export function sanityCheckTx(tx: PartialJsonTransaction) {
-  if (!tx?.id) {
-    throw new Error('Invalid transaction');
+  if (!tx.id) {
+    throw new Error("Invalid transaction: missing 'id'");
   }
 }
 
 export function sanityCheckChunk(chunk: JsonChunk) {
   if (!chunk.chunk) {
-    throw new Error('Chunk not defined');
+    throw new Error("Invalid chunk: missing 'chunk'");
   }
 
-  if (!chunk.data_path || !chunk.tx_path) {
-    throw new Error('Chunk metadata missing');
+  if (!chunk.tx_path) {
+    throw new Error("Invalid chunk: missing 'tx_path'");
+  }
+
+  if (!chunk.data_path) {
+    throw new Error("Invalid chunk: missing 'data_path'");
   }
 }
