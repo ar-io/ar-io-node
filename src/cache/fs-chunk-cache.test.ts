df4d6e39fc75c79402c07b24c1ee568725b6c6d2
dtfiedler <dylan@ar.io>
2022-09-22 14:35:54 -0600

    chore: add another test for chunk cache

--- src/cache/fs-chunk-cache.test.ts
+++ src/cache/fs-chunk-cache.test.ts
@@ -38,8 +38,8 @@ describe('FsChunkCache', () => {
   });
 
   describe('getChunkByRelativeOrAbsoluteOffset', () => {
-    it('should call chunkSource to retrieve chunk', async () => {
-      const spy = sandbox.spy(
+    it('should fetch chunk from the network', async () => {
+      const chunkSourceSpy = sandbox.spy(
         chunkSource,
         'getChunkByRelativeOrAbsoluteOffset',
       );
@@ -48,7 +48,7 @@ describe('FsChunkCache', () => {
         fromB64Url(B64_DATA_ROOT),
         0,
       );
-      expect(spy).to.have.been.called;
+      expect(chunkSourceSpy).to.have.been.called;
     });
   });
 
@@ -68,7 +68,7 @@ describe('FsChunkCache', () => {
       );
     });
 
-    it('should fetch from cache when available', async () => {
+    it('should fetch chunk data from cache when available', async () => {
       // mock the file exists
       const cacheGetSpy = sandbox
         .stub(chunkCache, 'getChunkData')
@@ -77,25 +77,16 @@ describe('FsChunkCache', () => {
         chunkCache,
         'getChunkByRelativeOrAbsoluteOffset',
       );
-      const readableChunk =
-        await chunkCache.getChunkDataByRelativeOrAbsoluteOffset(
-          ABSOLUTE_OFFSET,
-          fromB64Url(B64_DATA_ROOT),
-          0,
-        );
+      await chunkCache.getChunkDataByRelativeOrAbsoluteOffset(
+        ABSOLUTE_OFFSET,
+        fromB64Url(B64_DATA_ROOT),
+        0,
+      );
       expect(networkSpy).not.to.have.been.called;
       expect(cacheGetSpy).to.have.been.called;
-      const fetchedChunkData: any[] = [];
-      readableChunk.on('data', (d) => {
-        fetchedChunkData.push(d);
-      });
-      readableChunk.on('end', () => {
-        const fetchedBuffer = Buffer.concat(fetchedChunkData);
-        expect(fetchedBuffer).to.deep.equal(mockedChunkData);
-      });
     });
 
-    it('should fetch from network when not in local cache', async () => {
+    it('should fetch chunk data from network when not in local cache', async () => {
       // mock file does not exist
       const cacheHasSpy = sandbox
         .stub(chunkCache, 'hasChunkData')
@@ -112,8 +103,22 @@ describe('FsChunkCache', () => {
       expect(cacheGetSpy).to.have.been.called;
       expect(cacheHasSpy).to.have.been.called;
       expect(networkSpy).to.have.been.called;
-      const fetchedBuffer = fromB64Url(mockedJsonChunk.chunk);
-      expect(fetchedBuffer).to.deep.equal(mockedChunkData);
+    });
+
+    it('should fetch chunk data from network when an error occurs fetching from local cache', async () => {
+      const cacheHasSpy = sandbox.stub(chunkCache, 'hasChunkData').rejects();
+      const cacheGetSpy = sandbox.spy(chunkCache, 'getChunkData');
+      const networkSpy = sandbox
+        .stub(chunkCache, 'getChunkByRelativeOrAbsoluteOffset')
+        .resolves(mockedJsonChunk);
+      await chunkCache.getChunkDataByRelativeOrAbsoluteOffset(
+        ABSOLUTE_OFFSET,
+        fromB64Url(B64_DATA_ROOT),
+        0,
+      );
+      expect(cacheGetSpy).to.have.been.called;
+      expect(cacheHasSpy).to.have.been.called;
+      expect(networkSpy).to.have.been.called;
     });
   });
 });
