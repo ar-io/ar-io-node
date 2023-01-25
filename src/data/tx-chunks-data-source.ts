5d8fadae97fdfb15a81c884832e686eb951c89b1
David Whittington <djwhitt@gmail.com>
2023-01-25 10:00:09 -0600

    refactor(data chunks): remove unnecessary try/catch
    It's better to let the caller decide how to handle errors at this level.
    It knows more about the calling context and in the case of a composite
    source it can standardize logging across a range of other sources.

--- src/data/tx-chunks-data-source.ts
+++ src/data/tx-chunks-data-source.ts
@@ -31,71 +31,65 @@ export class TxChunksDataSource implements ContiguousDataSource {
   async getData(txId: string): Promise<ContiguousData> {
     this.log.debug('Fetching chunk data for TX', { txId });
 
-    try {
-      const [txDataRoot, txOffset] = await Promise.all([
-        this.chainSource.getTxField(txId, 'data_root'),
-        this.chainSource.getTxOffset(txId),
-      ]);
-      const size = +txOffset.size;
-      const offset = +txOffset.offset;
-      const startOffset = offset - size + 1;
-      let bytes = 0;
-      // we lose scope in the readable, so set to internal function
-      const getChunkDataByAny = (
-        absoluteOffset: number,
-        dataRoot: string,
-        relativeOffset: number,
-      ) =>
-        this.chunkSource.getChunkDataByAny(
-          size,
-          absoluteOffset,
-          dataRoot,
-          relativeOffset,
-        );
-      let chunkDataPromise: Promise<ChunkData> | undefined = getChunkDataByAny(
-        startOffset,
-        txDataRoot,
-        bytes,
-      );
-      const stream = new Readable({
-        autoDestroy: true,
-        read: async function () {
-          try {
-            if (!chunkDataPromise) {
-              this.push(null);
-              return;
-            }
+    const [txDataRoot, txOffset] = await Promise.all([
+      this.chainSource.getTxField(txId, 'data_root'),
+      this.chainSource.getTxOffset(txId),
+    ]);
+    const size = +txOffset.size;
+    const offset = +txOffset.offset;
+    const startOffset = offset - size + 1;
+    let bytes = 0;
 
-            const chunkData = await chunkDataPromise;
-            this.push(chunkData.chunk);
-            bytes += chunkData.chunk.length;
-
-            if (bytes < size) {
-              chunkDataPromise = getChunkDataByAny(
-                startOffset + bytes,
-                txDataRoot,
-                bytes,
-              );
-            } else {
-              chunkDataPromise = undefined;
-            }
-          } catch (error: any) {
-            this.destroy(error);
-          }
-        },
-      });
-      return {
-        stream,
+    // we lose scope in the readable, so set to internal function
+    const getChunkDataByAny = (
+      absoluteOffset: number,
+      dataRoot: string,
+      relativeOffset: number,
+    ) =>
+      this.chunkSource.getChunkDataByAny(
         size,
-        verified: true,
-      };
-    } catch (error: any) {
-      this.log.error('Failed to retrieve transaction data:', {
-        txId,
-        message: error.message,
-        stack: error.stack,
-      });
-      throw error;
-    }
+        absoluteOffset,
+        dataRoot,
+        relativeOffset,
+      );
+    let chunkDataPromise: Promise<ChunkData> | undefined = getChunkDataByAny(
+      startOffset,
+      txDataRoot,
+      bytes,
+    );
+
+    const stream = new Readable({
+      autoDestroy: true,
+      read: async function () {
+        try {
+          if (!chunkDataPromise) {
+            this.push(null);
+            return;
+          }
+
+          const chunkData = await chunkDataPromise;
+          this.push(chunkData.chunk);
+          bytes += chunkData.chunk.length;
+
+          if (bytes < size) {
+            chunkDataPromise = getChunkDataByAny(
+              startOffset + bytes,
+              txDataRoot,
+              bytes,
+            );
+          } else {
+            chunkDataPromise = undefined;
+          }
+        } catch (error: any) {
+          this.destroy(error);
+        }
+      },
+    });
+
+    return {
+      stream,
+      size,
+      verified: true,
+    };
   }
 }
