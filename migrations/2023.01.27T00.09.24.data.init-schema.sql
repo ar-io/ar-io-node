598e7aea930bc9719518546893ad65d5d8ef49fb
David Whittington <djwhitt@gmail.com>
2023-02-01 09:46:03 -0600

    feat(data caching): Add WIP contiguous data index tables
    Adds 3 new tables to a new 'data' DB - 'contiguous_data',
    'contiguous_data_ids', and 'data_roots'. 'contiguous_data' stores the
    original source content type and a hash of the data. The hash is used to
    locate the data in a store (filesystem, S3, etc.). Storing by hash
    ensures we don't unnecessarily store duplicate data. The other two
    tables associate the hashes with data IDs (TXs and data items for now),
    and data_roots respectively. Both tables record whether or not the
    content has been verified (TX signature verified or data_root calculated
    from chunks). Tracking and verifying content is VERY IMPORTANT for
    ensuring data integrity. Anything not verified should be considered
    suspect until it's verified. In the future we will add HTTP headers for
    verification status so that users can be aware of the verification
    status too.

---
+++ migrations/2023.01.27T00.09.24.data.init-schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS contiguous_data  (
+  hash BLOB PRIMARY KEY,
+  original_source_content_type TEXT,
+  create_at INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contiguous_data_ids (
+  id BLOB PRIMARY KEY,
+  contiguous_data_hash BLOB
+  content_type TEXT,
+  verified BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at INTEGER NOT NULL,
+  verified_at INTEGER
+);
+
+CREATE INDEX IF NOT EXISTS contiguous_data_ids_contiguous_data_hash_idx ON contiguous_data_ids (contiguous_data_hash);
+
+CREATE TABLE IF NOT EXISTS data_roots (
+  data_root BLOB PRIMARY KEY,
+  contiguous_data_hash BLOB
+  verified BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at INTEGER NOT NULL,
+  verified_at INTEGER
+);
+
+CREATE INDEX IF NOT EXISTS data_roots_contiguous_data_hash_idx ON data_roots (contiguous_data_hash);
---
+++ migrations/down/2023.01.27T00.09.24.data.init-schema.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS contiguous_data;
+DROP TABLE IF EXISTS contiguous_data_ids;
+DROP TABLE IF EXISTS data_roots;
