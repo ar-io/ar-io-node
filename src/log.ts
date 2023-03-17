35a4551fb3d2f38ca42cd420a7d58cb357af5cf8
David Whittington <djwhitt@gmail.com>
2023-03-17 14:54:58 -0500

    feat(logging): log INSTANCE_ID from the environment if it's set PE-3262
    Logs INSTANCE_ID from the environment as instanceID in the logs. This is
    when you're aggregating logs from multiple gateways.

--- src/log.ts
+++ src/log.ts
@@ -20,9 +20,14 @@ import { createLogger, format, transports } from 'winston';
 const LOG_LEVEL = process.env.LOG_LEVEL ?? 'info';
 const LOG_ALL_STACKTRACES = process.env.LOG_ALL_STACKTRACES === 'true';
 const LOG_FORMAT = process.env.LOG_FORMAT ?? 'simple';
+const INSTANCE_ID =
+  process.env.INSTANCE_ID === '' ? undefined : process.env.INSTANCE_ID;
 
 const logger = createLogger({
   level: LOG_LEVEL,
+  defaultMeta: {
+    instanceId: INSTANCE_ID,
+  },
   format: format.combine(
     format((info) => {
       // Only log stack traces when the log level is error or the
