b435fe7b2eeadcfbc1af974d0a6835a0c538bf24
David Whittington <djwhitt@gmail.com>
2023-04-24 14:45:33 -0500

    feat(bundles data): add tables to relate parent/child data PE-3477
    Adds two tables - one that relates data hashes to their parent hashes
    and another that relates ids to their parent ids. A parent hash/id in
    these tables means the data for the id (not the parent) is retrievable
    from the parent by reading a given amount of data starting at a
    particular offset in the parent. The hash table contains an offset but
    no size since the contiguous data hash table already includes the size.
    The id table includes both the offset and size since we don't record
    size for IDs (we rely on the related hash data size and we can have ids
    without hashes).

---
+++ migrations/2023.04.24T19.18.36.data.nested-data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS contiguous_data_parents (
+  hash BLOB NOT NULL,
+  parent_hash BLOB NOT NULL,
+  data_offset INTEGER NOT NULL,
+  PRIMARY KEY (hash, parent_hash)
+);
+
+CREATE TABLE IF NOT EXISTS contiguous_data_id_parents (
+  id BLOB NOT NULL,
+  parent_id BLOB NOT NULL,
+  data_offset INTEGER NOT NULL,
+  data_size INTEGER NOT NULL,
+  PRIMARY KEY (id, parent_id)
+);
