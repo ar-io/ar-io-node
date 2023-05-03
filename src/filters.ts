7685f2b4d4d18390af049113f5490e2203665fe9
David Whittington <djwhitt@gmail.com>
2023-05-03 16:51:43 -0500

    feat(ans-104): add configurable unbundling filters PE-3625
    Adds two new environment variables for configuring which ANS-104
    bundle transactions get unbundled and what data item data gets
    indexed (see the README for details).
    
    The environment variables contain JSON representing the filter. Filters
    are described by a map containing a single key, the match operator, and
    a value representing the arguments to those operators. Some match
    operators like the 'and' and 'or' operator accept arguments that are
    themselves filters. In those cases, the arguments are evaluated
    recursively as filters. If a match operator takes no arguments 'true' is
    used as the argument. The following filters are supported: always,
    never, and, or, tags, attributes.
    
    Example:
    
      {
        and: [
          {
            tags: [
              { name: "foo", value: "bar" }
            ]
          },
          {
            tags: [
              { name: "baz", valueStartsWith: "qux" }
            ]
          }
        ]
      }

--- src/filters.ts
+++ src/filters.ts
@@ -13,6 +13,38 @@ export class NeverMatch implements ItemFilter {
   }
 }
 
+export class MatchAll implements ItemFilter {
+  private readonly filters: ItemFilter[];
+
+  constructor(filters: ItemFilter[]) {
+    this.filters = filters;
+  }
+
+  async match(item: MatchableItem): Promise<boolean> {
+    const results = await Promise.all(
+      this.filters.map((filter) => filter.match(item)),
+    );
+
+    return results.every((result) => result);
+  }
+}
+
+export class MatchAny implements ItemFilter {
+  private readonly filters: ItemFilter[];
+
+  constructor(filters: ItemFilter[]) {
+    this.filters = filters;
+  }
+
+  async match(item: MatchableItem): Promise<boolean> {
+    const results = await Promise.all(
+      this.filters.map((filter) => filter.match(item)),
+    );
+
+    return results.some((result) => result);
+  }
+}
+
 type TagValueMatch = {
   name: string;
   value: string;
@@ -61,4 +93,77 @@ export class MatchTags implements ItemFilter {
 
     return false;
   }
+}
+
+export class MatchAttributes implements ItemFilter {
+  private readonly attributes: Partial<MatchableItem>;
+
+  constructor(attributes: Partial<MatchableItem>) {
+    this.attributes = attributes;
+  }
+
+  async match(item: MatchableItem): Promise<boolean> {
+    const matches: Set<string> = new Set();
+
+    for (const [name, value] of Object.entries(this.attributes)) {
+      if (item?.[name as keyof MatchableItem] === value) {
+        matches.add(name);
+      }
+    }
+
+    if (matches.size === Object.keys(this.attributes).length) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
+/**
+ * Examples:
+ *
+ *   {
+ *     tags: [
+ *        { name: "foo", value: "bar" },
+ *        { name: "baz", valueStartsWith: "qux" }
+ *      ]
+ *   }
+ *
+ *   {
+ *     and: [
+ *       {
+ *         tags: [
+ *           { name: "foo", value: "bar" }
+ *         ]
+ *       },
+ *       {
+ *         tags: [
+ *           { name: "baz", valueStartsWith: "qux" }
+ *         ]
+ *       }
+ *     ]
+ *   }
+ *
+ *   { never: true }
+ */
+export function createFilter(filter: any): ItemFilter {
+  if (filter === undefined || filter === '') {
+    return new NeverMatch();
+  }
+
+  if (filter?.tags) {
+    return new MatchTags(filter.tags);
+  } else if (filter?.attributes) {
+    return new MatchAttributes(filter.attributes);
+  } else if (filter?.and) {
+    return new MatchAll(filter.and.map(createFilter));
+  } else if (filter?.or) {
+    return new MatchAny(filter.or.map(createFilter));
+  } else if (filter?.never) {
+    return new NeverMatch();
+  } else if (filter?.always) {
+    return new AlwaysMatch();
+  }
+
+  throw new Error(`Invalid filter: ${filter}`);
 }
