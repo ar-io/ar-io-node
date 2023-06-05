a33811912d4b8268d861d80863d9fbbef5e108bb
David Whittington <djwhitt@gmail.com>
2023-06-05 15:23:20 -0500

    perf(sqlite graphql): add a transaction_id index on new_transaction_tags PE-3769
    Since we run a separate query to retrieve tags by transaction_id for
    each transaction returned, this substantially improves performance when
    querying new transactions (stable transactions already have a similar
    index).

---
+++ migrations/2023.06.05T18.32.29.core.new-transaction-tags-transaction-id-index.sql
@@ -0,0 +1,1 @@
+CREATE INDEX IF NOT EXISTS new_transaction_tags_transaction_id_idx ON new_transaction_tags (transaction_id);
---
+++ migrations/down/2023.06.05T18.32.29.core.new-transaction-tags-transaction-id-index.sql
@@ -0,0 +1,1 @@
+DROP INDEX IF EXISTS new_transaction_tags_transaction_id_idx;
