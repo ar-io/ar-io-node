c371fc025b4cb2a759d8dac7ca2b3ae60fa0f730
David Whittington <djwhitt@gmail.com>
2023-08-15 15:15:27 -0500

    fix(bundles): use data item content type when available PE-4404
    When bundles are indexed, the data items offsets are saved so that they
    can be used later to read data item data from the appropriate place in
    the contiguous L1 data. In order to respond with appropriate content
    type on that data we need to retrieve the content type from the data
    item index. Prior to this change we were incorrectly falling back to the
    default content type in that situation.

--- src/database/sql/core/data-attributes.sql
+++ src/database/sql/core/data-attributes.sql
@@ -8,5 +8,13 @@ FROM (
   SELECT data_root, data_size, content_type, false AS stable
   FROM new_transactions
   WHERE id = @id
+  UNION
+  SELECT null, data_size, content_type, true AS stable
+  FROM bundles.stable_data_items
+  WHERE id = @id
+  UNION
+  SELECT null, data_size, content_type, false AS stable
+  FROM bundles.new_data_items
+  WHERE id = @id
 )
 LIMIT 1
