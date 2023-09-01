c22647ffb8d1d4b1f5dc0643be2c0710f387a900
Hlöðver Sigurðsson <hlolli@gmail.com>
2023-09-01 14:27:53 +0200

    refactor: 'nix develop' flake
    * removed shell.nix
    * pinned nixpkgs
    * added flake-utils

--- flake.nix
+++ flake.nix
@@ -1,9 +1,29 @@
 {
   description = "AR.IO Node";
 
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      devShell.x86_64-linux = import ./shell.nix { inherit pkgs; };
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let pkgs = (import nixpkgs {
+          inherit system;
+        });
+        in {
+          devShells = {
+            default = pkgs.mkShell {
+              name = "ar-io-node-shell";
+              buildInputs = with pkgs; [
+                nodejs
+                sqlite-interactive
+              ];
+            };
+          };
+        }
+    );
+
+  nixConfig.bash-prompt = "\\e[32m\[ar-io-node-shell\]$\\e[0m ";
 }
