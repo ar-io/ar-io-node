c3615bb7b8db93a7470ad243ae043fc9b54d6448
David Whittington <djwhitt@gmail.com>
2023-11-27 15:14:32 -0600

    feat(docker): allow configuration of node GC old gen size PE-4903
    This allows users adjust the nodejs GC max old gen size. This can help
    avoid out of memory crashes when running a lot of workers to process
    GraphQL queries or do unbundling. This also raises the default to 2GB
    from 512MB.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,9 @@ mkdir -p data/sqlite
 # Migrate the DB to the latest version
 /nodejs/bin/node dist/migrate.js up
 
+if [ -z "$NODE_MAX_OLD_SPACE_SIZE" ]; then
+  NODE_MAX_OLD_SPACE_SIZE=2048
+fi
+
 # Run the gateway service
-exec /nodejs/bin/node dist/app.js
+exec /nodejs/bin/node --max-old-space-size=$NODE_MAX_OLD_SPACE_SIZE dist/app.js
