8a8e7be353841bd975ff9fd4a872ba9016d3ab42
bobinstein <cavanaugh.stephen1991@gmail.com>
2023-12-21 22:24:22 -0500

    fix: fixed issue with always true index filter

--- src/workers/webhook-emitter.ts
+++ src/workers/webhook-emitter.ts
@@ -2,13 +2,10 @@ import axios from 'axios';
 import * as EventEmitter from 'node:events';
 import * as winston from 'winston';
 
-import {
-  // ANS104_UNBUNDLE_FILTER,
-  ANS104_INDEX_FILTER,
-  WEBHOOK_TARGET_SERVER,
-} from '../config.js';
+import { ANS104_INDEX_FILTER, WEBHOOK_TARGET_SERVER } from '../config.js';
 import * as events from '../events.js';
 import { createFilter } from '../filters.js';
+import { AlwaysMatch } from '../filters.js';
 import { ItemFilter } from '../types.js';
 
 // WebhookEmitter class
@@ -16,15 +13,19 @@ export class WebhookEmitter {
   private eventEmitter: EventEmitter;
   private webhookTargetServer?: string;
   private log: winston.Logger;
-  // private unbundleFilter: ItemFilter;
   private indexFilter: ItemFilter;
 
   constructor(eventEmitter: EventEmitter, log: winston.Logger) {
     this.eventEmitter = eventEmitter;
     this.webhookTargetServer = WEBHOOK_TARGET_SERVER;
     this.log = log.child({ class: 'WebhookEmitter' });
-    // this.unbundleFilter = createFilter(ANS104_UNBUNDLE_FILTER);
-    this.indexFilter = createFilter(ANS104_INDEX_FILTER);
+
+    if (ANS104_INDEX_FILTER instanceof AlwaysMatch) {
+      this.indexFilter = ANS104_INDEX_FILTER;
+    } else {
+      this.indexFilter = createFilter(ANS104_INDEX_FILTER);
+    }
+
     this.registerEventListeners();
   }
   public shutdown(): void {
@@ -35,18 +36,14 @@ export class WebhookEmitter {
 
   private registerEventListeners(): void {
     this.eventEmitter.on(events.TX_INDEXED, async (tx) => {
-
       if (await this.indexFilter.match(tx)) {
         this.emitWebhook({ event: 'TX_INDEXED', data: tx });
-
       }
     });
 
     this.eventEmitter.on(events.ANS104_NESTED_BUNDLE_INDEXED, async (item) => {
-
       if (await this.indexFilter.match(item)) {
         this.emitWebhook({ event: 'ANS104_DATA_ITEM_INDEXED', data: item });
-
       }
     });
 
@@ -59,7 +56,7 @@ export class WebhookEmitter {
   }): Promise<void> {
     if (this.webhookTargetServer) {
       this.log.info(
-        `Emitting webhook to ${this.webhookTargetServer} for ${eventWrapper.event}`
+        `Emitting webhook to ${this.webhookTargetServer} for ${eventWrapper.event}`,
       );
       try {
         // Send a POST request to the webhookTargetServer with the eventWrapper
@@ -70,7 +67,9 @@ export class WebhookEmitter {
 
         // Check the response and handle it as needed
         if (response.status === 200) {
-          this.log.info('Webhook emitted successfully.');
+          this.log.info(
+            `Webhook emitted successfully for: ${eventWrapper.data.id}`,
+          );
         } else {
           this.log.error(
             `Failed to emit webhook. Status code: ${response.status}`,
@@ -80,9 +79,7 @@ export class WebhookEmitter {
         this.log.error('Error while emitting webhook:', error.message);
       }
     } else {
-      this.log.warn(
-        'Webhook target server is not defined. Webhook emission skipped.',
-      );
+      return;
     }
   }
 }
