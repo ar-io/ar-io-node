1aed4b35e0a83b0aa1a289dc9674f5c2364b2bf1
Karl Prieb <karl@ar.io>
2024-03-26 13:20:46 -0300

    test(end-to-end): testcontainers for end-to-end tests

--- src/store/lmdb-kv-store.test.ts
+++ src/store/lmdb-kv-store.test.ts
@@ -40,7 +40,7 @@ describe('LmdbKvStore', () => {
     await lmdbKvStore.set(key, value);
     const result = await lmdbKvStore.get(key);
     expect(result).not.to.be.undefined;
-    expect(toB64Url(result!)).to.equal('test'); // eslint-disable-line
+    expect(toB64Url(result!)).to.equal('test'); // eslint-disable-line @typescript-eslint/no-non-null-assertion
   });
 
   it('should properly delete buffer', async () => {
@@ -59,7 +59,7 @@ describe('LmdbKvStore', () => {
     await lmdbKvStore.set(key, Buffer.from('test2', 'base64url'));
     const result = await lmdbKvStore.get(key);
     expect(result).not.to.be.undefined;
-    expect(toB64Url(result!)).to.equal('test');
+    expect(toB64Url(result!)).to.equal('test'); // eslint-disable-line @typescript-eslint/no-non-null-assertion
   });
 
   it('should return a buffer when a Uint8Array is stored in the cache', async () => {
@@ -72,6 +72,6 @@ describe('LmdbKvStore', () => {
     const result = await lmdbKvStore.get(key);
     expect(result).not.to.be.undefined;
     expect(Buffer.isBuffer(result)).to.equal(true);
-    expect(toB64Url(result!)).to.equal('test');
+    expect(toB64Url(result!)).to.equal('test'); // eslint-disable-line @typescript-eslint/no-non-null-assertion
   });
 });
