234042fb40a68b4409e02d025e5a089411fd0354
Derek Sonnenberg <derek@ardrive.io>
2024-06-07 09:54:15 -0500

    test(bundler sidecar): init data post e2e test PE-6266

---
+++ test/end-to-end/bundler-sidecar.test.ts
@@ -0,0 +1,137 @@
+/**
+ * AR.IO Gateway
+ * Copyright (C) 2022-2023 Permanent Data Solutions, Inc. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import { strict as assert } from 'node:assert';
+import { after, before, describe, it } from 'node:test';
+import { rimraf } from 'rimraf';
+import {
+  DockerComposeEnvironment,
+  StartedDockerComposeEnvironment,
+  Wait,
+} from 'testcontainers';
+import wait from 'wait';
+import axios from 'axios';
+import Sqlite, { Database } from 'better-sqlite3';
+import { fromB64Url, sha256B64Url, toB64Url } from '../../src/lib/encoding.js';
+import { Environment } from 'testcontainers/build/types.js';
+import { createData } from 'arbundles';
+import Arweave from 'arweave';
+import { ArweaveSigner } from 'arbundles/src/signing/index.js';
+
+const projectRootPath = process.cwd();
+
+const cleanDb = () =>
+  rimraf(`${projectRootPath}/data/sqlite/*.db*`, { glob: true });
+const composeUp = async ({
+  START_HEIGHT = '1',
+  STOP_HEIGHT = '1',
+  ARNS_ROOT_HOST = 'ar-io.localhost',
+  ANS104_UNBUNDLE_FILTER = '{"always": true}',
+  ANS104_INDEX_FILTER = '{"always": true}',
+  ADMIN_API_KEY = 'secret',
+  ...ENVIRONMENT
+}: Environment = {}) => {
+  await cleanDb();
+  return new DockerComposeEnvironment(projectRootPath, 'docker-compose.yaml')
+    .withEnvironment({
+      START_HEIGHT,
+      STOP_HEIGHT,
+      ARNS_ROOT_HOST,
+      ANS104_UNBUNDLE_FILTER,
+      ANS104_INDEX_FILTER,
+      ADMIN_API_KEY,
+      DEBUG: 'testcontainers*',
+      ...ENVIRONMENT,
+    })
+    .withBuild()
+    .withWaitStrategy('upload-service-1', Wait.forHealthCheck())
+    .withWaitStrategy('core-1', Wait.forHealthCheck())
+    .withProfiles('bundler')
+    .up();
+};
+
+describe('Bundler Sidecar', () => {
+  let bundlesDb: Database;
+  let compose: StartedDockerComposeEnvironment;
+
+  const waitForIndexing = async () => {
+    const getAll = () =>
+      bundlesDb.prepare('SELECT * FROM new_data_items').all();
+
+    while (getAll().length === 0) {
+      console.log('Waiting for data items to be indexed...');
+      await wait(5000);
+    }
+  };
+
+  before(async () => {
+    compose = await composeUp();
+    bundlesDb = new Sqlite(`${projectRootPath}/data/sqlite/bundles.db`);
+  });
+
+  after(async () => {
+    await compose.down();
+    bundlesDb.close();
+  });
+
+  it('Verifying that S3DataSource can fetch data from S3', async () => {
+    const jwk = await Arweave.crypto.generateJWK();
+    const signer = new ArweaveSigner(jwk);
+    const dataItem = createData('test data', signer, {
+      tags: [{ name: 'Content-Type', value: 'text/plain' }],
+    });
+    await dataItem.sign(signer);
+
+    // post data to bundler
+    await axios({
+      method: 'post',
+      url: `http://envoy:${3000}/bundler/tx`,
+      headers: { 'Content-Type': 'application/octet-stream' },
+      data: dataItem.getRaw(),
+    });
+
+    // get data from gateway, should be instantly available
+    const res = await axios({
+      method: 'get',
+      url: `http://envoy:${3000}/${dataItem.id}`,
+    });
+
+    assert.equal(res.data, 'test data');
+
+    await waitForIndexing();
+
+    const stmt = bundlesDb.prepare('SELECT * FROM new_data_items');
+    const dataItems = stmt.all();
+
+    assert.equal(dataItems.length, 1);
+    const importedDataItem = dataItems[0];
+    assert.equal(toB64Url(importedDataItem.id), dataItem.id);
+    assert.equal(toB64Url(importedDataItem.parent_id), 'AA');
+    assert.equal(toB64Url(importedDataItem.root_transaction_id), 'AA');
+    assert.equal(toB64Url(importedDataItem.signature), dataItem.signature);
+    assert.equal(toB64Url(importedDataItem.anchor), dataItem.anchor);
+    assert.equal(toB64Url(importedDataItem.target), dataItem.target);
+    assert.equal(
+      toB64Url(importedDataItem.owner_address),
+      sha256B64Url(fromB64Url(dataItem.owner)),
+    );
+    assert.equal(importedDataItem.data_offset, 0);
+    assert.equal(importedDataItem.data_size, 9);
+    assert.equal(importedDataItem.tag_count, 1);
+    assert.equal(importedDataItem.content_type, 'text/plain');
+  });
+});
