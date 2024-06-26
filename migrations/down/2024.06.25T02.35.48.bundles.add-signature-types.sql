5b97ad8a734115d2ce51d506d3e126d82ea4158d
David Whittington <djwhitt@gmail.com>
2024-06-26 07:37:40 -0500

    fix(migrations): replace ADD with DROP in down migration

--- migrations/down/2024.06.25T02.35.48.bundles.add-signature-types.sql
+++ migrations/down/2024.06.25T02.35.48.bundles.add-signature-types.sql
@@ -1,3 +1,3 @@
-ALTER TABLE new_data_items ADD COLUMN signature_type INTEGER;
-ALTER TABLE stable_data_items ADD COLUMN signature_type INTEGER;
+ALTER TABLE new_data_items DROP COLUMN signature_type INTEGER;
+ALTER TABLE stable_data_items DROP COLUMN signature_type INTEGER;
 
