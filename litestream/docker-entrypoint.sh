5d2e4d6f3fdbf4ff97d2026dd477fa2434bf11dd
Hlöðver Sigurðsson <hlolli@gmail.com>
2024-07-15 13:44:14 +0200

    feat(litestream): add the ability to restore snapshots from backup

--- litestream/docker-entrypoint.sh
+++ litestream/docker-entrypoint.sh
@@ -7,4 +7,12 @@ ytt -f /etc/litestream.template.yaml --data-values-env TVAL >  /etc/litestream.y
 
 chmod go+r /etc/litestream.yml
 
+if [ -n $AR_IO_SQLITE_RESTORE_FROM_BACKUP ]; then
+  echo "Attempting to restore from backup if exists..."
+  /usr/local/bin/litestream restore -config /etc/litestream.yml -if-db-not-exists -if-replica-exists /app/data/sqlite/core.db
+  /usr/local/bin/litestream restore -config /etc/litestream.yml -if-db-not-exists -if-replica-exists /app/data/sqlite/data.db
+  /usr/local/bin/litestream restore -config /etc/litestream.yml -if-db-not-exists -if-replica-exists /app/data/sqlite/moderation.db
+  /usr/local/bin/litestream restore -config /etc/litestream.yml -if-db-not-exists -if-replica-exists /app/data/sqlite/bundles.db
+fi
+
 /usr/local/bin/litestream replicate -config /etc/litestream.yml
