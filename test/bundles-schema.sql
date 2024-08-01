14f46c614cbf0aac17e563cedcb8668f44947b18
Karl Prieb <karl@ar.io>
2024-08-01 13:23:23 -0300

    feat(sql): data items nullable signature migration

--- test/bundles-schema.sql
+++ test/bundles-schema.sql
@@ -2,34 +2,6 @@ CREATE TABLE wallets (
   address BLOB PRIMARY KEY,
   public_modulus BLOB
 );
-CREATE TABLE stable_data_items (
-  -- Identity
-  id BLOB NOT NULL,
-  parent_id BLOB NOT NULL,
-  root_transaction_id BLOB NOT NULL,
-  height INTEGER NOT NULL,
-  block_transaction_index INTEGER NOT NULL,
-  signature BLOB NOT NULL,
-  anchor BLOB NOT NULL,
-
-  -- Ownership
-  owner_address BLOB NOT NULL,
-  target BLOB,
-
-  -- Data
-  data_offset INTEGER NOT NULL,
-  data_size INTEGER NOT NULL,
-  content_type TEXT,
-
-  -- Metadata
-  tag_count INTEGER NOT NULL,
-  indexed_at INTEGER NOT NULL, signature_type INTEGER, offset INTEGER, size INTEGER, owner_offset INTEGER, owner_size INTEGER, signature_offset INTEGER, signature_size INTEGER, content_encoding TEXT,
-  PRIMARY KEY (id)
-);
-CREATE INDEX stable_data_items_height_block_transaction_index_id_idx ON stable_data_items (height, block_transaction_index, id);
-CREATE INDEX stable_data_items_target_height_block_transaction_index_id_idx ON stable_data_items (target, height, block_transaction_index, id);
-CREATE INDEX stable_data_items_owner_address_height_block_transaction_index_id_idx ON stable_data_items (owner_address, height, block_transaction_index, id);
-CREATE INDEX stable_data_items_parent_id_height_block_transaction_index_id_idx ON stable_data_items (parent_id, height, block_transaction_index, id);
 CREATE TABLE tag_names (
   hash BLOB PRIMARY KEY,
   name BLOB NOT NULL
@@ -99,13 +71,61 @@ CREATE INDEX bundles_index_filter_id_idx
 CREATE INDEX bundle_data_items_parent_id_filter_id_idx
   ON bundle_data_items (parent_id, filter_id);
 CREATE INDEX import_attempt_last_queued_idx ON bundles (import_attempt_count, last_queued_at);
-CREATE TABLE new_data_items (
+CREATE TABLE new_data_item_tags (
+  tag_name_hash BLOB NOT NULL,
+  tag_value_hash BLOB NOT NULL,
+  root_transaction_id BLOB,
+  data_item_id BLOB NOT NULL,
+  data_item_tag_index INTEGER NOT NULL,
+  height INTEGER,
+  indexed_at INTEGER NOT NULL,
+  PRIMARY KEY (tag_name_hash, tag_value_hash, root_transaction_id, data_item_id, data_item_tag_index)
+);
+CREATE INDEX new_data_item_tags_height_indexed_at_idx ON new_data_item_tags (height, indexed_at);
+CREATE INDEX new_data_item_tags_data_item_id_idx ON new_data_item_tags (data_item_id);
+CREATE TABLE IF NOT EXISTS "stable_data_items" (
+  -- Identity
+  id BLOB NOT NULL,
+  parent_id BLOB NOT NULL,
+  root_transaction_id BLOB NOT NULL,
+  height INTEGER NOT NULL,
+  block_transaction_index INTEGER NOT NULL,
+  signature BLOB, -- Changed to nullable
+  anchor BLOB NOT NULL,
+
+  -- Ownership
+  owner_address BLOB NOT NULL,
+  target BLOB,
+
+  -- Data
+  data_offset INTEGER NOT NULL,
+  data_size INTEGER NOT NULL,
+  content_type TEXT,
+
+  -- Metadata
+  tag_count INTEGER NOT NULL,
+  indexed_at INTEGER NOT NULL,
+  signature_type INTEGER,
+  offset INTEGER,
+  size INTEGER,
+  owner_offset INTEGER,
+  owner_size INTEGER,
+  signature_offset INTEGER,
+  signature_size INTEGER,
+  content_encoding TEXT,
+  PRIMARY KEY (id)
+);
+CREATE INDEX stable_data_items_height_block_transaction_index_id_idx ON stable_data_items (height, block_transaction_index, id);
+CREATE INDEX stable_data_items_target_height_block_transaction_index_id_idx ON stable_data_items (target, height, block_transaction_index, id);
+CREATE INDEX stable_data_items_owner_address_height_block_transaction_index_id_idx ON stable_data_items (owner_address, height, block_transaction_index, id);
+CREATE INDEX stable_data_items_parent_id_height_block_transaction_index_id_idx ON stable_data_items (parent_id, height, block_transaction_index, id);
+CREATE TABLE IF NOT EXISTS "new_data_items" (
   -- Identity
   id BLOB NOT NULL,
   parent_id BLOB,
   root_transaction_id BLOB,
   height INTEGER,
-  signature BLOB NOT NULL,
+  signature BLOB, -- Changed to nullable
   anchor BLOB NOT NULL,
 
   -- Ownership
@@ -119,7 +139,15 @@ CREATE TABLE new_data_items (
 
   -- Metadata
   tag_count INTEGER NOT NULL,
-  indexed_at INTEGER NOT NULL, signature_type INTEGER, offset INTEGER, size INTEGER, owner_offset INTEGER, owner_size INTEGER, signature_offset INTEGER, signature_size INTEGER, content_encoding TEXT,
+  indexed_at INTEGER NOT NULL,
+  signature_type INTEGER,
+  offset INTEGER,
+  size INTEGER,
+  owner_offset INTEGER,
+  owner_size INTEGER,
+  signature_offset INTEGER,
+  signature_size INTEGER,
+  content_encoding TEXT,
   PRIMARY KEY (id)
 );
 CREATE INDEX new_data_items_parent_id_id_idx ON new_data_items (parent_id, id);
@@ -127,18 +155,6 @@ CREATE INDEX new_data_items_root_transaction_id_id_idx ON new_data_items (root_t
 CREATE INDEX new_data_items_target_id_idx ON new_data_items (target, id);
 CREATE INDEX new_data_items_owner_address_id_idx ON new_data_items (owner_address, id);
 CREATE INDEX new_data_items_height_indexed_at_idx ON new_data_items (height, indexed_at);
-CREATE TABLE new_data_item_tags (
-  tag_name_hash BLOB NOT NULL,
-  tag_value_hash BLOB NOT NULL,
-  root_transaction_id BLOB,
-  data_item_id BLOB NOT NULL,
-  data_item_tag_index INTEGER NOT NULL,
-  height INTEGER,
-  indexed_at INTEGER NOT NULL,
-  PRIMARY KEY (tag_name_hash, tag_value_hash, root_transaction_id, data_item_id, data_item_tag_index)
-);
-CREATE INDEX new_data_item_tags_height_indexed_at_idx ON new_data_item_tags (height, indexed_at);
-CREATE INDEX new_data_item_tags_data_item_id_idx ON new_data_item_tags (data_item_id);
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 CREATE TABLE bundle_formats (
