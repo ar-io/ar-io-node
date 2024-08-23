229ada578e067cf229c52c15e6c75c571389d6d2
Karl Prieb <karl@ar.io>
2024-08-23 12:56:50 -0300

    fix(standalone-sqlite): remove signature from getDataAttributes

--- src/database/sql/core/data-attributes.sql
+++ src/database/sql/core/data-attributes.sql
@@ -1,19 +1,19 @@
 -- selectDataAttributes
 SELECT *
 FROM (
-  SELECT data_root, data_size, content_type, content_encoding, signature, true AS stable
+  SELECT data_root, data_size, content_type, content_encoding, true AS stable
   FROM stable_transactions
   WHERE id = @id
   UNION
-  SELECT data_root, data_size, content_type, content_encoding, signature, false AS stable
+  SELECT data_root, data_size, content_type, content_encoding, false AS stable
   FROM new_transactions
   WHERE id = @id
   UNION
-  SELECT null, data_size, content_type, content_encoding, signature, true AS stable
+  SELECT null, data_size, content_type, content_encoding, true AS stable
   FROM bundles.stable_data_items
   WHERE id = @id
   UNION
-  SELECT null, data_size, content_type, content_encoding, signature, false AS stable
+  SELECT null, data_size, content_type, content_encoding, false AS stable
   FROM bundles.new_data_items
   WHERE id = @id
 )
