7bb4892072d7ad608ef65d1bcf66b8c732addea4
dtfiedler <dylan@ar.io>
2024-09-12 14:55:46 -0600

    feat(PE-6738): introduce arns resolver GET endpoint
    This is the same behavior in the middleware, but does not fetch the data. Useful for those that just want resolution info for a name.

--- test/end-to-end/arns.test.ts
+++ test/end-to-end/arns.test.ts
@@ -110,4 +110,46 @@ describe('ArNS', function () {
     // Assert that the Location header matches the expected URL
     assert.strictEqual(res.headers['location'], expectedRedirect);
   });
+
+  // verify the /ar-io/resolver/:name endpoint
+  it('Verifying that /ar-io/resolver/:name returns 200 and resolution data', async function () {
+    const txId = 'TB2wJyKrPnkAW79DAwlJYwpgdHKpijEJWQfcwX715Co';
+    const res = await axios.get(
+      'http://localhost:4000/ar-io/resolver/ardrive',
+      {
+        headers: { Host: 'ardrive.ar-io.localhost' },
+      },
+    );
+
+    assert.strictEqual(res.status, 200);
+    assert.strictEqual(res.data.txId, txId);
+    assert.strictEqual(typeof res.data.ttlSeconds, 'number');
+    assert.strictEqual(typeof res.data.processId, 'string');
+  });
+
+  // verify the headers are set correctly on the response
+  it('Verifying that /ar-io/resolver/:name returns 200 and sets the correct headers', async function () {
+    const txId = 'TB2wJyKrPnkAW79DAwlJYwpgdHKpijEJWQfcwX715Co';
+    const res = await axios.get(
+      'http://localhost:4000/ar-io/resolver/ardrive',
+      {
+        headers: { Host: 'ardrive.ar-io.localhost' },
+      },
+    );
+
+    assert.strictEqual(typeof res.headers['x-arns-resolved-id'], txId);
+    assert.strictEqual(typeof res.headers['x-arns-ttl-seconds'], 'string');
+    assert.strictEqual(typeof res.headers['x-arns-process-id'], 'string');
+  });
+
+  it('Verifying that /ar-io/resolver/:name returns 404 for nonexistent name', async function () {
+    const res = await axios.get(
+      'http://localhost:4000/ar-io/resolver/nonexistent',
+      {
+        headers: { Host: 'ardrive.ar-io.localhost' },
+      },
+    );
+
+    assert.strictEqual(res.status, 404);
+  });
 });
