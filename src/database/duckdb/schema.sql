f5d4c01e29412d8394a62a694479cc70e1c04d8e
Karl Prieb <karl@ar.io>
2024-09-26 18:00:56 -0300

    chore(parquet-exporter): rename created_at to indexed_at in tags table

--- src/database/duckdb/schema.sql
+++ src/database/duckdb/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS tags (
   height UBIGINT NOT NULL,
   id BLOB NOT NULL,
   tag_index USMALLINT NOT NULL,
-  created_at UBIGINT,
+  indexed_at UBIGINT,
   tag_name BLOB NOT NULL,
   tag_value BLOB NOT NULL,
   is_data_item BOOLEAN NOT NULL
