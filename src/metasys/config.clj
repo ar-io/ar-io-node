952bc669e1de586f850c22588a3591461197ac45
David Whittington <djwhitt@gmail.com>
2024-10-25 13:06:10 -0500

    feat(metasys): support reading env vars in addition to .env-metasys

--- src/metasys/config.clj
+++ src/metasys/config.clj
@@ -1,10 +1,29 @@
 (ns metasys.config
   (:require [clojure.edn :as edn]
+            [clojure.string :as str]
             [metasys.util :as util]))
 
-(def env (util/read-env))
+(defn read-env
+  ([]
+   (read-env ".env.metasys"))
+  ([env-path]
+   (->> (slurp env-path)
+        (str/split-lines)
+        (remove #(str/starts-with? % "#")) ; Remove comments
+        (remove #(str/blank? %)) ; Remove blank lines
+        (map #(str/split % #"\s*=\s*")) ; Split key-value pairs
+        (map (fn [[k v]] [k (util/strip-quotes v)])) ; Strip value leading and trailing quotes
+        (into {}))))
+
+(def dot-env (read-env))
+
+(defn env [vname]
+  (or (System/getenv vname)
+      (dot-env vname)))
 
 (def oi-api-key (env "OI_API_KEY"))
 (def oi-base-url (env "OI_BASE_URL"))
 
-(def oi-knowledge (-> "oi-knowledge.edn" slurp edn/read-string))
+(def oi-knowledge (-> "oi-knowledge.edn"
+                      slurp
+                      edn/read-string))
--- src/metasys/util.clj
+++ src/metasys/util.clj
@@ -3,15 +3,3 @@
 
 (defn strip-quotes [s]
   (str/replace s #"^[\"](.*)[\"]$" "$1"))
-
-(defn read-env
-  ([]
-   (read-env ".env.metasys"))
-  ([env-path]
-   (->> (slurp env-path)
-        (str/split-lines)
-        (remove #(str/starts-with? % "#")) ; Remove comments
-        (remove #(str/blank? %)) ; Remove blank lines
-        (map #(str/split % #"\s*=\s*")) ; Split key-value pairs
-        (map (fn [[k v]] [k (strip-quotes v)])) ; Strip value leading and trailing quotes
-        (into {}))))
