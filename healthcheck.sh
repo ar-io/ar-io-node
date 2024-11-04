8883f4f5173777335b60814806a544b101a24e2c
Karl Prieb <karl@ar.io>
2024-11-04 15:56:22 -0300

    feat: data item indexing healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env sh
 
-exec /nodejs/bin/node -e 'require("http").get("http://localhost:4000/ar-io/healthcheck", (res) => { if(res.statusCode !== 200) process.exit(1); }).on("error", (err) => { process.exit(1); })'
+exec /nodejs/bin/node -e '
+require("http").get("http://localhost:4000/ar-io/healthcheck", (res) => {
+  if (res.statusCode !== 200) process.exit(1);
+  let data = "";
+  res.on("data", (chunk) => { data += chunk; });
+  res.on("end", () => {
+    try {
+      const json = JSON.parse(data);
+      if (json.status !== "ok") process.exit(1);
+    } catch (e) {
+      process.exit(1);
+    }
+  });
+}).on("error", () => { process.exit(1); });
+'
