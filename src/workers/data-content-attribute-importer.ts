3f613ec3f048a05bc2de4ca4b4e0638b7dfec391
Hlöðver Sigurðsson <hlolli@gmail.com>
2025-01-30 14:35:00 +0200

    fix: shutdown problems with db.stop when exit signal is received
      * remove all queue.drained calls in stop methods
      * better error handling around pool/worker stops in db.stop

--- src/workers/data-content-attribute-importer.ts
+++ src/workers/data-content-attribute-importer.ts
@@ -87,7 +87,6 @@ export class DataContentAttributeImporter {
   async stop(): Promise<void> {
     const log = this.log.child({ method: 'stop' });
     this.queue.kill();
-    await this.queue.drained();
     log.debug('Stopped successfully.');
   }
 
--- src/workers/transaction-fetcher.ts
+++ src/workers/transaction-fetcher.ts
@@ -118,7 +118,6 @@ export class TransactionFetcher {
   async stop(): Promise<void> {
     const log = this.log.child({ method: 'stop' });
     this.queue.kill();
-    await this.queue.drained();
     log.debug('Stopped successfully.');
   }
 
