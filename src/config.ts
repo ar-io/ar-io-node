a614ac0037265aecec2650951881243d703ea571
David Whittington <djwhitt@gmail.com>
2025-02-06 13:23:44 -0600

    feat(sqlite): disable writing transaction signatures by default
    This saves a lot of DB space, and we have a signature fetcher that will
    retrieve signatures from the trusted node for GraphQL results.

--- src/config.ts
+++ src/config.ts
@@ -234,7 +234,7 @@ export const WRITE_ANS104_DATA_ITEM_DB_SIGNATURES =
 
 // Whether or not to write the transaction signatures to the database
 export const WRITE_TRANSACTION_DB_SIGNATURES =
-  env.varOrDefault('WRITE_TRANSACTION_DB_SIGNATURES', 'true') === 'true';
+  env.varOrDefault('WRITE_TRANSACTION_DB_SIGNATURES', 'false') === 'true';
 
 // Whether or not to enable the data database WAL cleanup worker
 export const ENABLE_DATA_DB_WAL_CLEANUP =
