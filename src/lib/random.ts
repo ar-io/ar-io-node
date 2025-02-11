12fc6b3f0ea90d3e70434cf0ccecc63b8fd38523
Hlöðver Sigurðsson <hlolli@gmail.com>
2025-02-11 16:30:29 +0200

    feat: broadcast chunks to primary nodes in parallel
      * wrap primary chunk posting in a pLimit wrapper
      * shuffle the array of chunk post urls before each broadcasting
      * make a util function out of shuffleArray

---
+++ src/lib/random.ts
@@ -0,0 +1,28 @@
+/**
+ * AR.IO Gateway
+ * Copyright (C) 2022-2023 Permanent Data Solutions, Inc. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Fisher-Yates algorithm
+// this shuffles array in-place, use [...arr] pattern
+// to copy it beforehand if you don't want side-effects
+export const shuffleArray = <T>(array: T[]): T[] => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
