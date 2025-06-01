27431e208218963b608ae738e22c83ba3bce1936
David Whittington <djwhitt@gmail.com>
2025-06-01 18:09:50 -0500

    fix(verification): fix verification priority in coalesce PE-8125
    NULL should be coalesced to 0. The coalesce to 80 was left over from
    debugging.

--- src/database/sql/data/verification.sql
+++ src/database/sql/data/verification.sql
@@ -4,7 +4,7 @@ FROM contiguous_data_ids cd
 JOIN bundles.bundle_data_items bdi ON bdi.id = cd.id
 WHERE cd.verified = FALSE
   AND bdi.root_transaction_id IS NOT NULL
-  AND COALESCE(cd.verification_priority, 80) >= @min_verification_priority
+  AND COALESCE(cd.verification_priority, 0) >= @min_verification_priority
 ORDER BY cd.verification_priority DESC NULLS LAST, cd.verification_retry_count ASC NULLS FIRST, cd.id ASC
 LIMIT 1000;
 
