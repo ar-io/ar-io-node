4cae6e8e10b9547cfbab66c5ea003727e0b58bff
David Whittington <djwhitt@gmail.com>
2025-06-17 19:22:37 -0500

    feat: add experimental flush-to-stable script
    WARNING: This script is experimental and directly modifies databases use
    with caution.
    
    - Script mirrors StandaloneSqliteDatabase.flushStableDataItems logic
    - Add experimental bash script to flush stable chain and data item
      tables
    - Remove unused deleteStableDataItemsLessThanIndexedAt from
      bundles/cleanup.sql

--- src/database/sql/bundles/cleanup.sql
+++ src/database/sql/bundles/cleanup.sql
@@ -3,15 +3,11 @@ DELETE FROM new_data_items
 WHERE height < @height_threshold OR (
     height IS NULL AND
     indexed_at < @indexed_at_threshold
-  )
+  );
 
 -- deleteStaleNewDataItemTags
 DELETE FROM new_data_item_tags
 WHERE height < @height_threshold OR (
     height IS NULL AND
     indexed_at < @indexed_at_threshold
-  )
-
--- deleteStableDataItemsLessThanIndexedAt
-DELETE FROM stable_data_items
-WHERE indexed_at < @indexed_at_threshold
+  );
