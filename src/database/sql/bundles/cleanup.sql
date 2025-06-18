f1ff242f4f71a9f1b3a508f47ed88459ccef24f2
David Whittington <djwhitt@gmail.com>
2025-06-17 19:14:00 -0500

    refactor(sql): move data item prune statements into their own SQL file
    Moving prune statements into their own file so that flush.sql can be
    invoked as a standalone script.

--- src/database/sql/bundles/cleanup.sql
+++ src/database/sql/bundles/cleanup.sql
@@ -15,19 +15,3 @@ WHERE height < @height_threshold OR (
 -- deleteStableDataItemsLessThanIndexedAt
 DELETE FROM stable_data_items
 WHERE indexed_at < @indexed_at_threshold
-
--- deleteStableDataItemTagsWithHeightAndIndexedAt
-DELETE FROM stable_data_item_tags
-WHERE data_item_id IN (
-  SELECT id 
-  FROM stable_data_items
-  WHERE indexed_at < @indexed_at_threshold 
-    AND height >= @start_height 
-    AND height <= @end_height
-)
-
--- deleteStableDataItemsWithHeightAndIndexedAt
-DELETE FROM stable_data_items
-WHERE indexed_at < @indexed_at_threshold 
-  AND height >= @start_height 
-  AND height <= @end_height
