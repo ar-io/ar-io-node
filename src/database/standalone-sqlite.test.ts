ar-io/ar-io-node

commit 0bcccf6ef1d3e243cc84bde316f84ba6f689a9ec

feat(graphql): owner resolvers for clickhouse
Huge commit including several refactors and the new OwnerFetcher.

Added:
- return owner_size and owner_offset from clickhouse
- generic AttributeFetcher
- OwnerFetcher, resolves owner key from offsets or fetch from chain
- generic B64UrlStore
- OwnerStore
- generic JsonStore
- DataItemAttributesStore
- TransactionAttributesStore

Changed:
- SignatureFetcher based on AttributeFetcher
- SignatureStore

src/database/standalone-sqlite.ts
@@ -1067,6 +1067,8 @@ export class StandaloneSqliteDatabaseWorker {
       signature: row.signature ? toB64Url(row.signature) : null,
       signatureOffset: row.signature_offset,
       signatureSize: row.signature_size,
+      ownerOffset: row.owner_offset,
+      ownerSize: row.owner_size,
     };
   }
 
@@ -1078,9 +1080,9 @@ export class StandaloneSqliteDatabaseWorker {
     if (row === undefined) {
       return undefined;
     }
-
     return {
       signature: row.signature ? toB64Url(row.signature) : null,
+      owner: row.owner_key ? toB64Url(row.owner) : null,
     };
   }
 
@@ -1334,6 +1336,8 @@ export class StandaloneSqliteDatabaseWorker {
         'content_type',
         'owner_address',
         'public_modulus',
+        'CAST(NULL AS TEXT) AS owner_size',
+        'CAST(NULL AS TEXT) AS owner_offset',
         'nb.indep_hash AS block_indep_hash',
         'nb.block_timestamp AS block_timestamp',
         'nb.previous_block AS block_previous_block',
@@ -1371,6 +1375,8 @@ export class StandaloneSqliteDatabaseWorker {
         'content_type',
         'owner_address',
         'public_modulus',
+        'ndi.owner_size AS owner_size',
+        'ndi.owner_offset AS owner_offset',
         'nb.indep_hash AS block_indep_hash',
         'nb.block_timestamp AS block_timestamp',
         'nb.previous_block AS block_previous_block',
@@ -1408,6 +1414,8 @@ export class StandaloneSqliteDatabaseWorker {
         'content_type',
         'owner_address',
         'public_modulus',
+        'CAST(NULL AS TEXT) AS owner_size',
+        'CAST(NULL AS TEXT) AS owner_offset',
         'sb.indep_hash AS block_indep_hash',
         'sb.block_timestamp AS block_timestamp',
         'sb.previous_block AS block_previous_block',
@@ -1442,6 +1450,8 @@ export class StandaloneSqliteDatabaseWorker {
         'content_type',
         'owner_address',
         'public_modulus',
+        'sdi.owner_size AS owner_size',
+        'sdi.owner_offset AS owner_offset',
         'sb.indep_hash AS block_indep_hash',
         'sb.block_timestamp AS block_timestamp',
         'sb.previous_block AS block_previous_block',
@@ -1882,6 +1892,8 @@ export class StandaloneSqliteDatabaseWorker {
         recipient: tx.target ? toB64Url(tx.target) : null,
         ownerAddress: toB64Url(tx.owner_address),
         ownerKey: toB64Url(tx.public_modulus),
+        ownerSize: tx.owner_size,
+        ownerOffset: tx.owner_offset,
         fee: tx.reward,
         quantity: tx.quantity,
         dataSize: tx.data_size,
@@ -2001,6 +2013,8 @@ export class StandaloneSqliteDatabaseWorker {
         recipient: tx.target ? toB64Url(tx.target) : null,
         ownerAddress: toB64Url(tx.owner_address),
         ownerKey: toB64Url(tx.public_modulus),
+        ownerSize: tx.owner_size,
+        ownerOffset: tx.owner_offset,
         fee: tx.reward,
         quantity: tx.quantity,
         dataSize: tx.data_size,

src/data/attribute-fetcher.ts
@@ -1,0 +1,418 @@
+/**
+ * AR.IO Gateway
+ * Copyright (C) 2022-2023 Permanent Data Solutions, Inc. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import {
+  ContiguousDataSource,
+  ContiguousDataIndex,
+  ContiguousDataAttributes,
+  ChainSource,
+  TransactionAttributesStore,
+  DataItemAttributesStore,
+  DataItemAttributes,
+  TransactionAttributes,
+  B64UrlStore,
+} from '../types.js';
+import winston from 'winston';
+import { toB64Url } from '../lib/encoding.js';
+import { isEmptyString } from '../lib/string.js';
+
+export class AttributeFetcher {
+  protected log: winston.Logger;
+  protected dataSource: ContiguousDataSource;
+  protected dataIndex: ContiguousDataIndex;
+  private dataItemAttributesStore: DataItemAttributesStore;
+  private transactionAttributesStore: TransactionAttributesStore;
+
+  constructor({
+    log,
+    dataSource,
+    dataIndex,
+    dataItemAttributesStore,
+    transactionAttributesStore,
+  }: {
+    log: winston.Logger;
+    dataSource: ContiguousDataSource;
+    dataIndex: ContiguousDataIndex;
+    dataItemAttributesStore: DataItemAttributesStore;
+    transactionAttributesStore: TransactionAttributesStore;
+  }) {
+    this.log = log.child({ class: this.constructor.name });
+    this.dataSource = dataSource;
+    this.dataIndex = dataIndex;
+    this.dataItemAttributesStore = dataItemAttributesStore;
+    this.transactionAttributesStore = transactionAttributesStore;
+  }
+
+  async fetchDataFromParent({
+    parentId,
+    offset,
+    size,
+  }: {
+    parentId: string;
+    offset: number;
+    size: number;
+  }): Promise<string> {
+    const log = this.log.child({ method: 'fetchDataFromParent' });
+    log.debug('Fetching data from parent', { parentId, offset, size });
+
+    const { stream } = await this.dataSource.getData({
+      id: parentId,
+      dataAttributes: {
+        size,
+      } as ContiguousDataAttributes,
+      region: {
+        offset,
+        size,
+      },
+    });
+
+    let buffer = Buffer.alloc(0);
+
+    for await (const chunk of stream) {
+      buffer = Buffer.concat([buffer, chunk]);
+    }
+
+    return toB64Url(buffer);
+  }
+
+  async getDataItemAttributes(
+    id: string,
+  ): Promise<DataItemAttributes | undefined> {
+    const log = this.log.child({ method: 'getDataItemAttribute' });
+
+    let attributes = await this.dataItemAttributesStore.get(id);
+    if (attributes !== undefined) {
+      log.debug('Data item attributes found in store', { id });
+      return attributes;
+    }
+
+    attributes = await this.dataIndex.getDataItemAttributes(id);
+
+    if (attributes !== undefined) {
+      await this.dataItemAttributesStore.set(id, attributes);
+    }
+
+    return attributes;
+  }
+
+  async getTransactionAttributes(
+    id: string,
+  ): Promise<TransactionAttributes | undefined> {
+    const log = this.log.child({ method: 'getTransactionAttributes' });
+
+    let attributes = await this.transactionAttributesStore.get(id);
+    if (attributes !== undefined) {
+      log.debug('Transaction attributes found in store', { id });
+      return attributes;
+    }
+
+    attributes = await this.dataIndex.getTransactionAttributes(id);
+
+    if (attributes !== undefined) {
+      await this.transactionAttributesStore.set(id, attributes);
+    }
+
+    return attributes;
+  }
+}
+
+export class SignatureFetcher extends AttributeFetcher {
+  private chainSource: ChainSource;
+  private signatureStore: B64UrlStore;
+
+  constructor({
+    log,
+    dataSource,
+    dataIndex,
+    chainSource,
+    dataItemAttributesStore,
+    transactionAttributesStore,
+    signatureStore,
+  }: {
+    log: winston.Logger;
+    dataSource: ContiguousDataSource;
+    dataIndex: ContiguousDataIndex;
+    chainSource: ChainSource;
+    dataItemAttributesStore: DataItemAttributesStore;
+    transactionAttributesStore: TransactionAttributesStore;
+    signatureStore: B64UrlStore;
+  }) {
+    super({
+      log,
+      dataSource,
+      dataIndex,
+      dataItemAttributesStore,
+      transactionAttributesStore,
+    });
+    this.chainSource = chainSource;
+    this.signatureStore = signatureStore;
+  }
+
+  async getDataItemSignature({
+    id,
+    parentId,
+    signatureSize,
+    signatureOffset,
+  }: {
+    id: string;
+    parentId?: string;
+    signatureSize?: number;
+    signatureOffset?: number;
+  }): Promise<string | undefined> {
+    const log = this.log.child({ method: 'getDataItemSignature' });
+    log.debug('Fetching data item signature', { id });
+    const signature = await this.signatureStore.get(id);
+
+    if (signature !== undefined) {
+      log.debug('Data item signature fetched from store', { id });
+      return signature;
+    }
+
+    try {
+      if (
+        parentId === undefined ||
+        signatureSize === undefined ||
+        signatureOffset === undefined
+      ) {
+        const dataItemAttributes = await this.getDataItemAttributes(id);
+
+        if (dataItemAttributes === undefined) {
+          this.log.warn('No attributes found for data item', { id });
+          return undefined;
+        }
+
+        if (typeof dataItemAttributes.signature === 'string') {
+          await this.signatureStore.set(id, dataItemAttributes.signature);
+
+          return dataItemAttributes.signature;
+        }
+
+        parentId = dataItemAttributes.parentId;
+        signatureSize = dataItemAttributes.signatureSize;
+        signatureOffset = dataItemAttributes.signatureOffset;
+      }
+
+      const signature = await this.fetchDataFromParent({
+        parentId,
+        offset: signatureOffset,
+        size: signatureSize,
+      });
+
+      await this.signatureStore.set(id, signature);
+
+      return signature;
+    } catch (error) {
+      log.error('Error fetching data item signature', {
+        id,
+        error: (error as Error).message,
+      });
+
+      return undefined;
+    }
+  }
+
+  async getTransactionSignature({
+    id,
+  }: {
+    id: string;
+  }): Promise<string | undefined> {
+    const log = this.log.child({ method: 'getTransactionSignature' });
+    log.debug('Fetching transaction signature', { id });
+
+    const signature = await this.signatureStore.get(id);
+
+    if (signature !== undefined) {
+      log.debug('Transaction signature fetched from store', { id });
+      return signature;
+    }
+
+    try {
+      const transactionAttributes = await this.getTransactionAttributes(id);
+
+      if (transactionAttributes === undefined) {
+        this.log.warn('No attributes found for transaction', { id });
+      }
+
+      if (typeof transactionAttributes?.signature === 'string') {
+        await this.signatureStore.set(id, transactionAttributes.signature);
+
+        return transactionAttributes.signature;
+      }
+
+      const signatureFromChain = await this.chainSource.getTxField(
+        id,
+        'signature',
+      );
+
+      if (typeof signatureFromChain === 'string') {
+        await this.signatureStore.set(id, signatureFromChain);
+
+        return signatureFromChain;
+      }
+
+      return undefined;
+    } catch (error) {
+      log.error('Error fetching transaction signature', {
+        id,
+        error: (error as Error).message,
+      });
+
+      return undefined;
+    }
+  }
+}
+
+export class OwnerFetcher extends AttributeFetcher {
+  private chainSource: ChainSource;
+  private ownerStore: B64UrlStore;
+
+  constructor({
+    log,
+    dataSource,
+    dataIndex,
+    chainSource,
+    dataItemAttributesStore,
+    transactionAttributesStore,
+    ownerStore,
+  }: {
+    log: winston.Logger;
+    dataSource: ContiguousDataSource;
+    dataIndex: ContiguousDataIndex;
+    chainSource: ChainSource;
+    dataItemAttributesStore: DataItemAttributesStore;
+    transactionAttributesStore: TransactionAttributesStore;
+    ownerStore: B64UrlStore;
+  }) {
+    super({
+      log,
+      dataSource,
+      dataIndex,
+      dataItemAttributesStore,
+      transactionAttributesStore,
+    });
+    this.chainSource = chainSource;
+    this.ownerStore = ownerStore;
+  }
+
+  async getDataItemOwner({
+    id,
+    parentId,
+    ownerSize,
+    ownerOffset,
+  }: {
+    id: string;
+    parentId?: string;
+    ownerSize?: number;
+    ownerOffset?: number;
+  }): Promise<string | undefined> {
+    const log = this.log.child({ method: 'getDataItemOwner' });
+    log.debug('Fetching data item owner', { id });
+
+    const owner = await this.ownerStore.get(id);
+
+    if (owner !== undefined) {
+      log.debug('Data item owner fetched from store', { id });
+      return owner;
+    }
+
+    try {
+      if (
+        parentId === undefined ||
+        ownerSize === undefined ||
+        ownerOffset === undefined
+      ) {
+        const dataItemAttributes = await this.getDataItemAttributes(id);
+
+        if (dataItemAttributes === undefined) {
+          this.log.warn('No attributes found for data item', { id });
+          return undefined;
+        }
+
+        parentId = dataItemAttributes.parentId;
+        ownerSize = dataItemAttributes.ownerSize;
+        ownerOffset = dataItemAttributes.ownerOffset;
+      }
+
+      const owner = await this.fetchDataFromParent({
+        parentId,
+        offset: ownerOffset,
+        size: ownerSize,
+      });
+
+      await this.ownerStore.set(id, owner);
+      return owner;
+    } catch (error) {
+      log.error('Error fetching data item owner', {
+        id,
+        error: (error as Error).message,
+      });
+
+      return undefined;
+    }
+  }
+
+  async getTransactionOwner({
+    id,
+  }: {
+    id: string;
+  }): Promise<string | undefined> {
+    const log = this.log.child({ method: 'getTransactionOwner' });
+    log.debug('Fetching transaction owner', { id });
+
+    const owner = await this.ownerStore.get(id);
+
+    if (owner !== undefined) {
+      log.debug('Transaction owner fetched from store', { id });
+      return owner;
+    }
+
+    try {
+      const transactionAttributes = await this.getTransactionAttributes(id);
+
+      if (
+        transactionAttributes !== undefined &&
+        typeof transactionAttributes.owner === 'string'
+      ) {
+        await this.ownerStore.set(id, transactionAttributes.owner);
+
+        return transactionAttributes.owner;
+      }
+
+      this.log.warn('No attributes found for transaction', { id });
+
+      const ownerFromChain = await this.chainSource.getTxField(id, 'owner');
+
+      if (
+        typeof ownerFromChain === 'string' &&
+        !isEmptyString(ownerFromChain)
+      ) {
+        await this.ownerStore.set(id, ownerFromChain);
+
+        return ownerFromChain;
+      }
+
+      return undefined;
+    } catch (error) {
+      log.error('Error fetching transaction signature', {
+        id,
+        error: (error as Error).message,
+      });
+
+      return undefined;
+    }
+  }
+}

src/data/signature-fetcher.test.ts
@@ -1,218 +1,0 @@
-/**
- * AR.IO Gateway
- * Copyright (C) 2022-2023 Permanent Data Solutions, Inc. All Rights Reserved.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-import { strict as assert } from 'node:assert';
-import { describe, it, beforeEach, mock } from 'node:test';
-import * as winston from 'winston';
-import { SignatureFetcher } from './signature-fetcher.js';
-import {
-  ContiguousDataSource,
-  ContiguousDataIndex,
-  ChainSource,
-  SignatureStore,
-} from '../types.js';
-
-describe('SignatureFetcher', () => {
-  let log: winston.Logger;
-  let dataSource: ContiguousDataSource;
-  let dataIndex: ContiguousDataIndex;
-  let chainSource: ChainSource;
-  let signatureFetcher: SignatureFetcher;
-  let signatureStore: SignatureStore;
-
-  beforeEach(() => {
-    log = winston.createLogger({ silent: true });
-    dataSource = {
-      getData: mock.fn(),
-    } as unknown as ContiguousDataSource;
-    dataIndex = {
-      getDataItemAttributes: mock.fn(),
-      getTransactionAttributes: mock.fn(),
-    } as unknown as ContiguousDataIndex;
-    chainSource = {
-      getTxField: mock.fn(),
-    } as unknown as ChainSource;
-    signatureStore = {
-      get: mock.fn(),
-      set: mock.fn(),
-    } as unknown as SignatureStore;
-
-    signatureFetcher = new SignatureFetcher({
-      log,
-      dataSource,
-      dataIndex,
-      chainSource,
-      signatureStore,
-    });
-  });
-
-  describe('getDataItemSignature', () => {
-    it('should return undefined if no attributes found', async () => {
-      mock.method(dataIndex, 'getDataItemAttributes', async () => undefined);
-
-      const result = await signatureFetcher.getDataItemSignature('testId');
-
-      assert.strictEqual(result, undefined);
-    });
-
-    it('should return signature from signature store if it exists', async () => {
-      mock.method(signatureStore, 'get', async () => 'signature-from-store');
-
-      const result = await signatureFetcher.getDataItemSignature('testId');
-
-      assert.strictEqual(result, 'signature-from-store');
-    });
-
-    it('should return signature if it exists in attributes', async () => {
-      const testSignature = 'testSignature';
-      mock.method(dataIndex, 'getDataItemAttributes', async () => ({
-        signature: testSignature,
-      }));
-
-      const result = await signatureFetcher.getDataItemSignature('testId');
-
-      assert.strictEqual(result, testSignature);
-    });
-
-    it('should fetch and return signature if not in attributes', async () => {
-      const testParentId = 'testParentId';
-      const testSignatureOffset = 100;
-      const testSignatureSize = 512;
-      const testSignatureBuffer = Buffer.from('testSignature');
-
-      mock.method(dataIndex, 'getDataItemAttributes', async () => ({
-        parentId: testParentId,
-        signatureOffset: testSignatureOffset,
-        signatureSize: testSignatureSize,
-      }));
-
-      mock.method(dataSource, 'getData', async () => ({
-        stream: {
-          [Symbol.asyncIterator]: async function* () {
-            yield testSignatureBuffer;
-          },
-        },
-      }));
-
-      const result = await signatureFetcher.getDataItemSignature('testId');
-
-      assert.strictEqual(result, testSignatureBuffer.toString('base64url'));
-    });
-
-    it('should fetch and return signature if parentId, signatureOffset, signatureSize is provided', async () => {
-      const testParentId = 'testParentId';
-      const testSignatureOffset = '100';
-      const testSignatureSize = '512';
-      const testSignatureBuffer = Buffer.from('testSignature');
-
-      mock.method(dataSource, 'getData', async () => ({
-        stream: {
-          [Symbol.asyncIterator]: async function* () {
-            yield testSignatureBuffer;
-          },
-        },
-      }));
-
-      const result = await signatureFetcher.getDataItemSignature(
-        'testId',
-        testParentId,
-        testSignatureOffset,
-        testSignatureSize,
-      );
-
-      assert.strictEqual(result, testSignatureBuffer.toString('base64url'));
-    });
-
-    it('should handle errors and return undefined', async () => {
-      mock.method(dataIndex, 'getDataItemAttributes', async () => {
-        throw new Error('Test error');
-      });
-
-      const result = await signatureFetcher.getDataItemSignature('testId');
-
-      assert.strictEqual(result, undefined);
-    });
-  });
-
-  describe('getTransactionSignature', () => {
-    it('should return signature from signature store if it exists', async () => {
-      mock.method(signatureStore, 'get', async () => 'signature-from-store');
-
-      const result = await signatureFetcher.getDataItemSignature('testId');
-
-      assert.strictEqual(result, 'signature-from-store');
-    });
-
-    it('should return signature if it exists in attributes', async () => {
-      const testSignature = 'testSignature';
-      mock.method(dataIndex, 'getTransactionAttributes', async () => ({
-        signature: testSignature,
-      }));
-
-      const result = await signatureFetcher.getTransactionSignature('testId');
-
-      assert.strictEqual(result, testSignature);
-    });
-
-    it('should fetch and return signature from chain if no attributes found', async () => {
-      const testChainSignature = 'testChainSignature';
-      mock.method(dataIndex, 'getTransactionAttributes', async () => undefined);
-      mock.method(chainSource, 'getTxField', async () => testChainSignature);
-
-      const result = await signatureFetcher.getTransactionSignature('testId');
-
-      assert.strictEqual(result, testChainSignature);
-    });
-
-    it('should fetch and return signature from chain if not in attributes', async () => {
-      const testId = 'testId';
-      const testChainSignature = 'testChainSignature';
-
-      mock.method(dataIndex, 'getTransactionAttributes', async () => ({
-        signature: undefined,
-      }));
-
-      mock.method(chainSource, 'getTxField', async () => testChainSignature);
-
-      const result = await signatureFetcher.getTransactionSignature(testId);
-
-      assert.strictEqual(result, testChainSignature);
-    });
-
-    it('should return undefined if signature not found in attributes or chain', async () => {
-      mock.method(dataIndex, 'getTransactionAttributes', async () => ({
-        signature: undefined,
-      }));
-
-      mock.method(chainSource, 'getTxField', async () => undefined);
-
-      const result = await signatureFetcher.getTransactionSignature('testId');
-
-      assert.strictEqual(result, undefined);
-    });
-
-    it('should handle errors and return undefined', async () => {
-      mock.method(dataIndex, 'getTransactionAttributes', async () => {
-        throw new Error('Test error');
-      });
-
-      const result = await signatureFetcher.getTransactionSignature('testId');
-
-      assert.strictEqual(result, undefined);
-    });
-  });
-});

commit 3212ec5b6c73cc31c0376eb3d2cc10793e6ecf6b

fix(database): return wallet owner in getTransactionAttributes (PE-9087)
`StandaloneSqliteDatabaseWorker.getTransactionAttributes` gated the
owner field on `row.owner_key`, but `selectTransactionAttributes` only
projects `signature` and `owner` (aliased from `w.public_modulus`).
`row.owner_key` was always undefined, so owner was always `null` even
when wallets.public_modulus had the RSA-2048 key. Result:
`OwnerFetcher.getTransactionOwner` always fell through to
`chainSource.getTx({txId})` → arweave.net for L1 transactions, even
though the local DB had the data sitting in the wallets table.
Combined with the GraphQL cancellation work in this branch, this is
the dominant driver of the 21–65% client-disconnect rate observed on
the gw2 canary: owner queries for L1 txs were paying full upstream
latency for data that was already local.

The bug was introduced in commit 0bcccf6e ("feat(graphql): owner
resolvers for clickhouse") and went undetected because:
- `standalone-sqlite.test.ts` had no tests for
  `getTransactionAttributes` — the SQL→object boundary was uncovered.
- `attribute-fetchers.test.ts` mocks `dataIndex.getTransactionAttributes`,
  so the buggy production path was never exercised.
- `resolvers.test.ts` fixtures populate inline `signature`/`ownerKey`,
  so resolveTxSignature/resolveTxOwnerKey hit early-return paths
  before invoking the fetcher.

Add a regression test block covering the four cases at the
SQL→object seam: owner present, owner missing, signature null,
unknown id.

Co-Authored-By: Claude Opus 4.7 (1M context) <noreply@anthropic.com>

src/database/standalone-sqlite.ts
@@ -1226,7 +1226,13 @@ export class StandaloneSqliteDatabaseWorker {
     }
     return {
       signature: row.signature ? toB64Url(row.signature) : null,
-      owner: row.owner_key ? toB64Url(row.owner) : null,
+      // The SQL projects `w.public_modulus AS owner` (see
+      // src/database/sql/core/transaction-attributes.sql); there is no
+      // `owner_key` column. Prior to this fix the gate was `row.owner_key`
+      // which is always undefined → `owner` was always null even when the
+      // wallets table had public_modulus, forcing OwnerFetcher to fall
+      // through to chainSource for every L1-tx owner query.
+      owner: row.owner ? toB64Url(row.owner) : null,
     };
   }
 

src/database/standalone-sqlite.test.ts
@@ -343,6 +343,91 @@ describe('StandaloneSqliteDatabase', () => {
     });
   });
 
+  describe('getTransactionAttributes', () => {
+    // Regression coverage for a defect introduced in commit 0bcccf6e where
+    // the worker checked `row.owner_key` (a column the SQL never returns)
+    // instead of `row.owner`. The bug always reported owner=null even when
+    // wallets.public_modulus was populated, forcing OwnerFetcher to fall
+    // through to chainSource for every L1-tx owner query. No prior test
+    // exercised the SQL→object boundary; AttributeFetcher tests mocked the
+    // dataIndex and the resolver tests used fixtures with inline ownerKey,
+    // so the typo went undetected. Keep these tests focused on that seam.
+    const id = '_H6KgmI_ZfSdSlf9r2xzDh_ebJnvQtTYLUBQlnRjIdM';
+    const ownerAddress = Buffer.alloc(32, 0xab);
+    const publicModulus = Buffer.alloc(512, 0xcd);
+    const signature = Buffer.alloc(512, 0xef);
+
+    const insertTx = (sigValue: Buffer | null) => {
+      coreDb
+        .prepare(
+          `INSERT OR REPLACE INTO stable_transactions
+            (id, height, block_transaction_index, format, last_tx,
+             owner_address, signature, quantity, reward, tag_count,
+             offset, data_size)
+           VALUES (@id, 1, 0, 2, @last_tx,
+             @owner_address, @signature, '0', '0', 0,
+             0, 0)`,
+        )
+        .run({
+          id: fromB64Url(id),
+          last_tx: Buffer.alloc(32),
+          owner_address: ownerAddress,
+          signature: sigValue,
+        });
+    };
+
+    const insertWallet = () => {
+      coreDb
+        .prepare(
+          `INSERT OR REPLACE INTO wallets (address, public_modulus)
+           VALUES (@address, @public_modulus)`,
+        )
+        .run({ address: ownerAddress, public_modulus: publicModulus });
+    };
+
+    beforeEach(() => {
+      coreDb.prepare(`DELETE FROM stable_transactions WHERE id = @id`).run({
+        id: fromB64Url(id),
+      });
+      coreDb
+        .prepare(`DELETE FROM wallets WHERE address = @address`)
+        .run({ address: ownerAddress });
+    });
+
+    it('returns the wallet public_modulus as owner when joined', () => {
+      insertTx(signature);
+      insertWallet();
+
+      const attrs = dbWorker.getTransactionAttributes(id);
+      assert.equal(attrs?.owner, toB64Url(publicModulus));
+      assert.equal(attrs?.signature, toB64Url(signature));
+    });
+
+    it('returns owner=null when wallets.public_modulus is missing', () => {
+      // No wallet row inserted — the LEFT JOIN produces a row with
+      // owner=NULL even though the tx exists.
+      insertTx(signature);
+
+      const attrs = dbWorker.getTransactionAttributes(id);
+      assert.equal(attrs?.owner, null);
+      assert.equal(attrs?.signature, toB64Url(signature));
+    });
+
+    it('returns signature=null when stable_transactions.signature is NULL', () => {
+      insertTx(null);
+      insertWallet();
+
+      const attrs = dbWorker.getTransactionAttributes(id);
+      assert.equal(attrs?.signature, null);
+      assert.equal(attrs?.owner, toB64Url(publicModulus));
+    });
+
+    it('returns undefined when the transaction is unknown', () => {
+      const attrs = dbWorker.getTransactionAttributes(id);
+      assert.equal(attrs, undefined);
+    });
+  });
+
   describe('saveBlockAndTxs', () => {
     it('should insert the block in the new_blocks table', async () => {
       const height = 982575;